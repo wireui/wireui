f1d89ab0c4c3d59d7c0b4fa4a5035b53856673d2
PH7-Jack <pedrolivertwd@gmail.com>
2021-02-11 18:26:51 -0300

    feat: add heroicons solid and outline

--- src/resources/views/components/icon.blade.php
+++ src/resources/views/components/icon.blade.php
@@ -1,5 +1,8 @@
-@props(['name', 'size' => 5])
+@props([
+    'style' => config('wireui.icons.style'),
+    'size'  => config('wireui.icons.size'),
+    'name',
+])
 
-<x-dynamic-component
-    :component="\PH7JACK\WireUi\WireUiServiceProvider::PACKAGE_NAME.'.icons.'.$name"
+<x-dynamic-component component="wireui::icons.{{ $style }}.{{ $name }}"
     {{ $attributes->merge(['class' => 'h-.'.$size.' w-'.$size]) }} />
