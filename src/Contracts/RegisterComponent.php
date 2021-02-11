e21bb4dafee0e3372386ce73f1d5531e8fec1a57
PH7-Jack <pedrolivertwd@gmail.com>
2021-02-11 18:40:18 -0300

    chore: refactoration
    - remove unused translations
    - remove unused classes

--- src/Contracts/RegisterComponent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PH7JACK\WireUi\Contracts;
-
-interface RegisterComponent
-{
-    public static function register(): void;
-}
--- src/resources/lang/en/messages.php
+++ src/resources/lang/en/messages.php
@@ -1,13 +1,3 @@
 <?php
 
-return [
-    'save'                => 'Save',
-    'invalid_hour'        => 'Hour format is invalid',
-    'invalid_minutes'     => 'Minutes format is invalid',
-    'invalid_time_format' => 'Time format is invalid',
-    'confirm'             => 'Are you sure?',
-    'confirm_delete'      => 'Do you want to delete?',
-    'danger_alert'        => 'You won\'t be able to revert this!',
-    'yes'                 => 'Yes',
-    'no'                  => 'No',
-];
+return [];
