0287df3af80c4b2bdb76def5412e2a7eea43af38
PH7-Jack <pedrolivertwd@gmail.com>
2021-02-10 22:04:42 -0300

    chore: add wireui config

--- src/config/wireui.php
+++ src/config/wireui.php
@@ -1,3 +1,17 @@
 <?php
 
-return [];
+return [
+    /*
+        |--------------------------------------------------------------------------
+        | Icons
+        |--------------------------------------------------------------------------
+        |
+        | The icons config will be used in icon component as default
+        | https://heroicons.com
+        |
+    */
+    'icons' => [
+        'style' => env('WIREUI_ICONS_STYLE', 'outline'),
+        'size'  => env('WIREUI_ICONS_SIZE', 5),
+    ],
+];
