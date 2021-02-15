6c88fb2471f9f0d5b865e647a48e4b867357a880
PH7-Jack <pedrolivertwd@gmail.com>
2021-02-15 01:09:46 -0300

    chore: split publishes

--- src/WireUiServiceProvider.php
+++ src/WireUiServiceProvider.php
@@ -13,11 +13,18 @@ class WireUiServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__ . '/resources/views', self::PACKAGE_NAME);
         $this->loadTranslationsFrom(__DIR__ . '/resources/lang', self::PACKAGE_NAME);
         $this->mergeConfigFrom(__DIR__ . '/config/wireui.php', self::PACKAGE_NAME);
+
         $this->publishes([
             __DIR__ . '/config/wireui.php' => config_path('wireui.php'),
-            __DIR__ . '/resources/views'   => resource_path('views/vendor/' . self::PACKAGE_NAME),
-            __DIR__ . '/resources/lang'    => resource_path('lang/vendor/' . self::PACKAGE_NAME),
-        ], self::PACKAGE_NAME);
+        ], self::PACKAGE_NAME . '.config');
+
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('views/vendor/' . self::PACKAGE_NAME),
+        ], self::PACKAGE_NAME . '.resources');
+
+        $this->publishes([
+            __DIR__ . '/resources/lang' => resource_path('lang/vendor/' . self::PACKAGE_NAME),
+        ], self::PACKAGE_NAME . '.lang');
     }
 
     public function register()
