502a32f4a151807e109382ad0366233f748b65de
PH7-Jack <pedrolivertwd@gmail.com>
2021-03-14 18:30:05 -0300

    chore: fix route to prevent icon cache without style

--- app/Http/Controllers/IconsController.php
+++ app/Http/Controllers/IconsController.php
@@ -4,14 +4,13 @@ namespace WireUi\App\Http\Controllers;
 
 class IconsController extends Controller
 {
-    public function getIcon(string $icon, ?string $style = null)
+    public function getIcon(string $style, string $icon)
     {
-        $style    = $style ?: config('wireui.icons.style');
         $iconPath = __DIR__ . "/../../../resources/assets/heroicons/{$style}/{$icon}.svg";
 
         return response()->file($iconPath, [
             'Content-Type'  => 'image/svg+xml; charset=utf-8',
-            'Cache-Control' => 'public, max-age=31536000',
+            'Cache-Control' => 'public, only-if-cached',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use WireUi\App\Http\Controllers\IconsController;
 
-Route::get('/wireui/icons/{icon}/{style?}', [IconsController::class, 'getIcon'])->name('wireui.icons');
+Route::get('/wireui/icons/{style}/{icon}', [IconsController::class, 'getIcon'])
+    ->where('style', '(outline|solid)')
+    ->name('wireui.icons');
