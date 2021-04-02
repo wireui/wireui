959082159442e5c0a5d26c2aad9a2017e078b24f
PH7-Jack <pedrolivertwd@gmail.com>
2021-04-02 15:22:49 -0300

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './wireui/resources/ts/**/*.js',
+    './resources/views/**/*.blade.php'
+  ],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,13 @@
 const mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss')
 
-mix.ts('ts/index.ts', 'dist/wireui.js').setPublicPath('dist')
+mix.ts('ts/index.ts', 'dist/wireui.js')
+  .setPublicPath('dist')
+  .sass('resources/scss/wireui.scss', 'dist')
+  .options({
+    processCssUrls: false,
+    postCss: [tailwindcss('./tailwind.config.js')]
+  })
 
 if (mix.inProduction()) {
   mix.version()
