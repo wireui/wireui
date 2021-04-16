abe3aaa59c75d82e04b50bbacc2ce59ee1480dd6
PH7-Jack <pedrolivertwd@gmail.com>
2021-04-16 01:43:28 -0300

    chore: justify prefix; fix append when has error

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -21,12 +21,14 @@
 
     <div class="mt-1 relative rounded-md shadow-sm">
         @if ($prefix || $icon)
-            <div class="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none
+            <div class="absolute inset-y-0 left-0 pl-2.5 flex items-center pointer-events-none
                 {{ $hasError ? 'text-red-500' : 'text-gray-400' }}">
                 @if ($icon)
                     <x-wireui::icon :name="$icon" class="h-5 w-5" />
                 @elseif($prefix)
-                    {{ $prefix }}
+                    <span class="w-5 flex items-center justify-center font-semibold">
+                        {{ $prefix }}
+                    </span>
                 @endif
             </div>
         @elseif($prepend)
@@ -39,13 +41,15 @@
             'autocomplete' => 'off',
         ]) }} />
 
-        @if ($hasError || $suffix || $rightIcon)
-            <div class="absolute inset-y-0 right-0 pr-2 flex items-center pointer-events-none
+        @if ($suffix || $rightIcon || ($hasError && !$append))
+            <div class="absolute inset-y-0 right-0 pr-2.5 flex items-center pointer-events-none
                 {{ $hasError ? 'text-red-500' : 'text-gray-400' }}">
                 @if ($rightIcon)
                     <x-wireui::icon :name="$rightIcon" class="h-5 w-5" />
                 @elseif($suffix)
-                    {{ $suffix }}
+                    <span class="w-5 flex items-center justify-center font-semibold">
+                        {{ $suffix }}
+                    </span>
                 @elseif($hasError)
                     <x-wireui::icon name="exclamation-circle" class="h-5 w-5" />
                 @endif
