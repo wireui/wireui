cb6f6eadeabf84c9e6709f75ea2bd02e4829fe7c
PH7-Jack <pedrolivertwd@gmail.com>
2021-05-11 22:58:05 -0300

    feat: add img prop on user option

--- resources/views/components/select/user-option.blade.php
+++ resources/views/components/select/user-option.blade.php
@@ -5,7 +5,7 @@
     :readonly="$readonly"
     :option="$option">
     <div class="flex items-center gap-x-3">
-        <img src="{{ $option['img'] }}" class="flex-shrink-0 h-6 w-6 rounded-full">
+        <img src="{{ data_get($option, 'img', $img) }}" class="flex-shrink-0 h-6 w-6 rounded-full">
         {{ $label }}
     </div>
 </x-select.option>
--- src/View/Components/Select/UserOption.php
+++ src/View/Components/Select/UserOption.php
@@ -4,6 +4,21 @@ namespace WireUi\View\Components\Select;
 
 class UserOption extends Option
 {
+    public ?string $img;
+
+    public function __construct(
+        bool $readonly = false,
+        bool $disabled = false,
+        ?string $label = null,
+        ?string $value = null,
+        ?string $img = null,
+        $option = null
+    ) {
+        parent::__construct($readonly, $disabled, $label, $value, $option);
+
+        $this->img = $img;
+    }
+
     public function render()
     {
         return view('wireui::components.select.user-option');
