2c7b3b2e48b6b6a6d587adda36adff10508e3d7d
Alberto <info@albertoperipolli.com>
2021-06-09 10:17:19 +0200

    Add it translation

---
+++ resources/lang/it/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'empty_options' => 'Nessuna Opzione',
+    'datePicker.months' => "['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']",
+    'datePicker.days' => "['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab']",
+];
