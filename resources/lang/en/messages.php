58c39227193848037c380dfae2bc17eacc118188
Alberto Peripolli <info@albertoperipolli.com>
2021-06-09 11:50:31 +0200

    - datepicker tips translation

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-    'empty_options'     => 'Empty Options',
-    'datePicker.months' => "['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']",
-    'datePicker.days'   => "['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']",
+    'empty_options' => 'Empty Options',
+    'datePicker' => [
+        'months' => "['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']",
+        'days' => "['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']",
+        'tomorrow' => 'Tomorrow',
+        'today' => 'Today',
+        'yesterday' => 'Yesterday'
+    ]
 ];
--- resources/lang/it/messages.php
+++ resources/lang/it/messages.php
@@ -2,6 +2,11 @@
 
 return [
     'empty_options' => 'Nessuna Opzione',
-    'datePicker.months' => "['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']",
-    'datePicker.days' => "['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab']",
+    'datePicker' => [
+        'months' => "['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']",
+        'days' => "['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab']",
+        'tomorrow' => 'Domani',
+        'today' => 'Oggi',
+        'yesterday' => 'Ieri'
+    ]
 ];
