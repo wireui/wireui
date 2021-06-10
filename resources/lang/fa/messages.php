946692bca95cc2f30150250ef01892f335948cb5
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-06-10 05:55:31 +0430

    Add Persian Language.

---
+++ resources/lang/fa/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'empty_options' => 'بدون گزینه',
+    'datePicker' => [
+        'months' => "['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']",
+        'days' => "['یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنج شنبه', 'جمعه', 'شنبه']",
+        'tomorrow' => 'فردا',
+        'today' => 'امروز',
+        'yesterday' => 'دیروز'
+    ]
+];
