78a0e7f4cb1ad49bbd9ec423b2fa0ad310a70eb4
PH7-Jack <pedrolivertwd@gmail.com>
2021-07-04 23:19:16 -0300

    feat: add checkbox dark mode

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -3,14 +3,17 @@
         @if ($leftLabel)
             <span class="mr-2 text-sm font-medium">{{ $leftLabel }}</span>
         @endif
+
         <input {{ $attributes->class([
-                'block sm:text-sm rounded-md transition ease-in-out duration-100 focus:outline-none',
-                'text-primary-600 focus:ring-primary-600',
-                'w-5 h-5' => $md,
-                'w-6 h-6' => $lg,
-                'ring-negative-500 ring-2 ring-offset-2 border-negative-400' => $errors->has($name)
+            'block sm:text-sm rounded-sm transition ease-in-out duration-100 focus:outline-none',
+            'border-secondary-300 text-primary-600 checked:bg-primary-600 focus:ring-primary-600',
+            'dark:border-secondary-600 dark:text-secondary-600 dark:focus:ring-0 dark:bg-secondary-600',
+            'w-5 h-5' => $md,
+            'w-6 h-6' => $lg,
+            'ring-negative-500 ring-2 ring-offset-2 border-negative-400' => $errors->has($name)
             ])->merge(['type' => 'checkbox']) }}
         />
+
         @if ($label)
             <span class="ml-2 text-sm font-medium">{{ $label }}</span>
         @endif
