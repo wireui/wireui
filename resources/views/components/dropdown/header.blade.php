a3a994e6bb4df137675a1f051960f45fe110f088
PH7-Jack <pedrolivertwd@gmail.com>
2021-07-04 23:38:17 -0300

    feat: add dropdown dark mode

--- resources/views/components/dropdown/header.blade.php
+++ resources/views/components/dropdown/header.blade.php
@@ -1,5 +1,5 @@
 <div class="@if($separator) border-t border-secondary-200 @endif">
-    <h6 {{ $attributes->merge(['class' => 'block pl-2 pt-2 pb-1 text-xs text-secondary-600 sticky top-0 bg-white']) }}>
+    <h6 {{ $attributes->merge(['class' => $classes]) }}>
         {{ $label }}
     </h6>
 
--- src/View/Components/Dropdown/DropdownItem.php
+++ src/View/Components/Dropdown/DropdownItem.php
@@ -32,6 +32,7 @@ class DropdownItem extends Component
     {
         return Str::of('text-secondary-600 px-4 py-2 text-sm flex items-center cursor-pointer rounded-md')
             ->append(' transition-colors duration-150 hover:text-secondary-900 hover:bg-secondary-100')
+            ->append(' dark:text-secondary-400 dark:hover:bg-secondary-700')
             ->when($this->separator, fn(Stringable $str) => $str->append(' border-t border-secondary-200'));
     }
 }
