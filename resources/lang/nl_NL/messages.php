5c64a802b3831f77f831bb57babbd6062a0bafca
bvbunnik <Bramvanbunnik@gmail.com>
2021-08-19 10:08:22 +0100

    Added Dutch translation

---
+++ resources/lang/nl_NL/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'empty_options' => 'Lege Opties',
+    'datePicker'    => [
+        'months'    => "['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']",
+        'days'      => "['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za']",
+        'tomorrow'  => 'Morgen',
+        'today'     => 'Vandaag',
+        'yesterday' => 'Gisteren',
+    ],
+    'errors' => [
+        'title' => 'Er zaten {errors} fouten in uw inzending',
+    ],
+];
