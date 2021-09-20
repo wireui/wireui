76efe6a3bc7d8c511d07fc704dea14ae9996ccd8
PH7-Jack <pedrolivertwd@gmail.com>
2021-09-20 00:37:22 -0300

    refactor currency component

--- resources/views/components/inputs/currency.blade.php
+++ resources/views/components/inputs/currency.blade.php
@@ -1,60 +1,11 @@
-<div x-data="{
-    model: @entangle($attributes->wire('model')),
-    input: null,
-    config: {
-        isLazy: @boolean($attributes->wire('model')->hasModifier('lazy')),
-        thousands: '{{ $thousands }}',
-        decimal:   '{{ $decimal }}',
-        precision:  {{ $precision }},
-        emitFormatted: @boolean($emitFormatted),
-    },
-
-    mask(currency, emitInput = true) {
-        if (typeof currency === 'number') {
-            currency = currency.toString()
-        }
-
-        if (
-            currency?.endsWith(this.config.decimal)
-            && $wireui.utils.occurrenceCount(currency, this.config.decimal) === 1
-        ) {
-            if (currency.length === 1) {
-                return this.input = `0.${this.config.decimal}`
-            }
-
-            return
-        }
-
-        this.input = $wireui.utils.currency.mask(currency, this.config)
-
-        if (!this.config.isLazy && emitInput) {
-            this.emitInput(this.input)
-        }
-    },
-    unMask(currency) {
-        return $wireui.utils.currency.unMask(currency, this.config)
-    },
-    emitInput(currency) {
-        this.model = this.config.emitFormatted
-            ? currency
-            : this.unMask(currency)
-    },
-}"
-x-init="function() {
-    if (typeof this.model !== 'object') {
-        this.mask(this.model)
-    }
-
-    $watch('model', currency => {
-        if (!this.config.emitFormatted) {
-            currency = $wireui.utils.currency.mask(currency, this.config)
-        }
-
-        if (currency !== this.input) {
-            this.mask(currency, false)
-        }
-    })
-}">
+<div x-data="currency({
+    isLazy: @boolean($attributes->wire('model')->hasModifier('lazy')),
+    model:  @entangle($attributes->wire('model')),
+    emitFormatted: @boolean($emitFormatted),
+    thousands: '{{ $thousands }}',
+    decimal:   '{{ $decimal }}',
+    precision:  {{ $precision }},
+})">
     <x-input {{ $attributes->whereDoesntStartWith('wire:model')->except('type') }}
         :borderless="$borderless"
         :shadowless="$shadowless"
--- ts/components/index.ts
+++ ts/components/index.ts
@@ -1,6 +1,7 @@
 import { Alpine } from './alpine'
 import dropdown from './dropdown'
 import maskable from './inputs/maskable'
+import currency from './inputs/currency'
 
 export interface Start {
   (Alpine: Alpine): void
@@ -9,6 +10,7 @@ export interface Start {
 const start: Start = (Alpine: Alpine) => {
   Alpine.data('dropdown', dropdown)
   Alpine.data('maskable', maskable)
+  Alpine.data('currency', currency)
 }
 
 export default start
