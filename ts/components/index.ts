ec98755af24e1f0753e8c6865346afad6dc8c38c
PH7-Jack <pedrolivertwd@gmail.com>
2021-09-22 00:50:39 -0300

    refactor notifications

--- ts/components/index.ts
+++ ts/components/index.ts
@@ -2,6 +2,7 @@ import { Alpine } from './alpine'
 import dropdown from './dropdown'
 import modal from './modal'
 import dialog from './dialog'
+import notifications from './notifications'
 import maskable from './inputs/maskable'
 import currency from './inputs/currency'
 
@@ -13,6 +14,7 @@ const start: Start = (Alpine: Alpine) => {
   Alpine.data('wireui_dropdown', dropdown)
   Alpine.data('wireui_modal', modal)
   Alpine.data('wireui_dialog', dialog)
+  Alpine.data('wireui_notifications', notifications)
   Alpine.data('wireui_inputs_maskable', maskable)
   Alpine.data('wireui_inputs_currency', currency)
 }
---
+++ ts/components/notifications.ts
@@ -0,0 +1,96 @@
+import { parseConfirmation, parseNotification } from '../notifications/parses'
+import { timer } from '../notifications/timer'
+import uuid from '../utils/uuid'
+
+export interface Notifications {
+  [index: string]: any
+}
+
+export default (): Notifications => ({
+  notifications: [],
+
+  init () {
+    window.Wireui.dispatchHook('notifications:load')
+  },
+  proccessNotification (notification) {
+    notification.id = uuid()
+
+    if (notification.timeout) {
+      notification.timer = timer(
+        notification.timeout,
+        () => {
+          notification.onClose()
+          notification.onTimeout()
+          this.removeNotification(notification.id)
+        },
+        (percentage) => {
+          const progressBar = document.getElementById(`timeout.bar.${notification.id}`)
+
+          if (!progressBar) return
+
+          progressBar.style.width = `${percentage}%`
+        }
+      )
+    }
+
+    this.notifications.push(notification)
+
+    if (notification.icon) {
+      this.$nextTick(() => {
+        this.fillNotificationIcon(notification)
+      })
+    }
+  },
+  addNotification ({ options, componentId }) {
+    const notification = parseNotification(options, componentId)
+
+    this.proccessNotification(notification)
+  },
+  addConfirmNotification ({ options, componentId }) {
+    const notification = parseConfirmation(options, componentId)
+
+    this.proccessNotification(notification)
+  },
+  fillNotificationIcon (notification) {
+    const classes = `w-6 h-6 ${notification.icon.color}`.split(' ')
+
+    fetch(`/wireui/icons/outline/${notification.icon.name}`)
+      .then(response => response.text())
+      .then(text => {
+        const svg = new DOMParser().parseFromString(text, 'image/svg+xml').documentElement
+
+        svg.classList.add(...classes)
+
+        document
+          ?.getElementById(`notification.${notification.id}`)
+          ?.querySelector('.notification-icon')
+          ?.replaceChildren(svg)
+      })
+  },
+  removeNotification (id) {
+    const index = this.notifications.findIndex(notification => notification.id === id)
+
+    if (~index) { this.notifications.splice(index, 1) }
+  },
+  closeNotification (notification) {
+    notification.onClose()
+    notification.onDismiss()
+    this.removeNotification(notification.id)
+  },
+  pauseNotification (notification) {
+    notification.timer?.pause()
+  },
+  resumeNotification (notification) {
+    notification.timer?.resume()
+  },
+  accept (notification) {
+    notification.onClose()
+    notification.accept.execute()
+    this.removeNotification(notification.id)
+  },
+  reject (notification) {
+    notification.onClose()
+    notification.reject.execute()
+    this.removeNotification(notification.id)
+  }
+})
