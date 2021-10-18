d47c1739a20e66ca4657f21cb5a7d95aa2c6d639
Arthur Monney <monneylobe@gmail.com>
2021-10-18 21:14:48 +0100

    :globe_with_meridians: Add french translations

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'empty_options' => 'Aucune Option',
+    'datePicker'    => [
+        'months'    => "['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']",
+        'days'      => "['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam']",
+        'tomorrow'  => 'Demain',
+        'today'     => 'Aujourd\'ui',
+        'yesterday' => 'Hier',
+    ],
+    'errors' => [
+        'title' => 'Il y a eu {errors} erreurs durant votre soumission',
+    ],
+];
