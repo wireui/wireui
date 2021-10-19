08836d007cd2e827ba0cb7e53ea788291cf7395b
Daniel Ang <daniel@sysanalyst.eu>
2021-10-19 11:48:57 +0200

    Catalan translation

---
+++ resources/lang/ca/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'empty_options' => 'Opcions buides',
+    'datePicker'    => [
+        'months'    => "['gener', 'febrer', 'març', 'abril', 'maig', 'juny', 'juliol', 'agost', 'setembre', 'octubre', 'novembre', 'desembre']",
+        'days'      => "['dg', 'dl', 'dt', 'dc', 'dj', 'dv', 'ds']",
+        'tomorrow'  => 'Demà',
+        'today'     => 'Avui',
+        'yesterday' => 'Ahir',
+    ],
+    'errors' => [
+        'title' => "S'han trobat {errors} errors de validació",
+    ],
+];
