c0f3e3e2b7d257bc9a89988702d77a16b577d0cb
PH7-Jack <pedrolivertwd@gmail.com>
2021-11-03 09:31:36 -0300

    adjust native select classes

--- src/View/Components/NativeSelect.php
+++ src/View/Components/NativeSelect.php
@@ -45,7 +45,7 @@ class NativeSelect extends FormComponent
 
     public function defaultClasses(): string
     {
-        return 'block w-full pl-3 pr-10 py-2 text-base sm:text-sm shadow-sm
+        return 'form-select block w-full pl-3 pr-10 py-2 text-base sm:text-sm shadow-sm
                 rounded-md border bg-white focus:ring-1 focus:outline-none
                 dark:bg-secondary-800 dark:border-secondary-600 dark:text-secondary-400';
     }
@@ -55,7 +55,7 @@ class NativeSelect extends FormComponent
         return 'border-secondary-300 focus:ring-primary-500 focus:border-primary-500';
     }
 
-    public function errorClasses()
+    public function errorClasses(): string
     {
         return 'border-negative-400 focus:ring-negative-500 focus:border-negative-500 text-negative-500
                 dark:border-negative-600 dark:text-negative-500';
