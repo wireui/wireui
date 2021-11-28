2c67f93adf19b2de0124649b06fccc235af2d4f8
PH7-Jack <pedrolivertwd@gmail.com>
2021-11-28 17:07:27 -0300

    feature: add wire:key on maskable

--- resources/views/components/inputs/maskable.blade.php
+++ resources/views/components/inputs/maskable.blade.php
@@ -3,7 +3,7 @@
     model: @entangle($attributes->wire('model')),
     emitFormatted: @boolean($emitFormatted),
     mask: {{ $mask }},
-})">
+})" {{ $attributes->only('wire:key') }}>
     <x-dynamic-component
         :component="WireUiComponent::resolve('input')"
         :borderless="$borderless"
@@ -20,6 +20,6 @@
         x-model="input"
         x-on:input="onInput($event.target.value)"
         x-on:blur="emitInput"
-        {{ $attributes->whereDoesntStartWith(['wire:model', 'x-model']) }}
+        {{ $attributes->whereDoesntStartWith(['wire:model', 'x-model', 'wire:key']) }}
     />
 </div>
