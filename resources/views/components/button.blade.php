41a240e6b8a991e22850d4d0aa6e9da3f47cc0bb
Zulkifli Mohamed <mr.putera@gmail.com>
2021-12-24 08:51:43 +0800

    Add wire loading delay to the button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -29,7 +29,7 @@ wire:loading.attr="disabled">
             @if (preg_replace('/[^a-zA-Z]+/', '', $spinner))
                 wire:target="{{ $spinner }}"
             @endif
-            wire:loading>
+            wire:loading.delay>
             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
             <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
