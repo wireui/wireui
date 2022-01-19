06b71d561c7572ed6c3891491a2cd82e4f6b23a7
Zulkifli Mohamed <mr.putera@gmail.com>
2022-01-19 20:25:10 +0800

    Add malay language

---
+++ resources/lang/ms/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'empty_options' => 'Tiada rekod dijumpai.',
+    'datePicker'    => [
+        'months'    => "['Januari', 'Februari', 'Mar', 'April', 'Mei', 'Jun', 'Julai', 'Ogos', 'September', 'Oktober', 'November', 'Disember']",
+        'days'      => "['Ahd', 'Isn', 'Sel', 'Rab', 'Kha', 'Jum', 'Sab']",
+        'tomorrow'  => 'Esok',
+        'today'     => 'Hari Ini',
+        'yesterday' => 'Semalam',
+    ],
+    'errors' => [
+        'title' => 'Terdapat ralat {errors} berlaku',
+    ],
+];
