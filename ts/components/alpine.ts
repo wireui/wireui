0b3f24836ffbb131b8c0f55a24b06664fb3305ac
PH7-Jack <pedrolivertwd@gmail.com>
2022-04-27 00:29:49 -0300

    feat: add alpine typescript integration
    - add component interface
    - add fake base alpine component

--- ts/components/alpine.ts
+++ ts/components/alpine.ts
@@ -3,3 +3,13 @@ export type Entangle = any
 export interface Alpine {
   data (name: string, data: any): void
 }
+
+export interface Component {
+  $el: HTMLElement
+  $root: HTMLElement
+  $watch: (name: string, callback: CallableFunction) => void
+  $nextTick: (callback: CallableFunction) => void
+  $dispatch: (name: string, value: any) => void
+}
+
+export const baseComponent = {} as Component
