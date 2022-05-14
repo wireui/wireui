867a0a002c38b2abdaf1513cfdd117b9b20fdaed
PH7-Jack <pedrolivertwd@gmail.com>
2022-05-14 20:16:14 -0300

    seup routes and base path in unit tests

--- tests/Unit/UnitTestCase.php
+++ tests/Unit/UnitTestCase.php
@@ -2,11 +2,24 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Facades\Route;
 use Orchestra\Testbench\TestCase;
 use WireUi\Providers\WireUiServiceProvider;
 
 class UnitTestCase extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Route::middleware('web')->group(base_path('src/routes/web.php'));
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app->setBasePath(__DIR__ . '/../..');
+    }
+
     protected function getPackageProviders($app)
     {
         return [WireUiServiceProvider::class];
