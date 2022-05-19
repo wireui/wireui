956d14ab73f842fd48fa81899bcfeebc63e54395
PH7-Jack <pedrolivertwd@gmail.com>
2022-05-19 04:56:00 -0300

    remove button outline

--- src/View/Components/BaseButton.php
+++ src/View/Components/BaseButton.php
@@ -84,8 +84,8 @@ abstract class BaseButton extends Component
     private function mergeClasses(ComponentAttributeBag $attributes): ComponentAttributeBag
     {
         return $attributes->class([
-            'focus:outline-none inline-flex justify-center items-center',
-            'transition-all ease-in duration-100 focus:ring-2 focus:ring-offset-2',
+            'outline-none inline-flex justify-center items-center',
+            'transition-all ease-in duration-150 focus:ring-2 focus:ring-offset-2',
             'hover:shadow-sm disabled:opacity-80 disabled:cursor-not-allowed',
             'rounded-full' => !$this->squared && $this->rounded,
             'rounded'      => !$this->squared && !$this->rounded,
