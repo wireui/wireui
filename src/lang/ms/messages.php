32b2237a6f0116d9085dfda64d3c0425b6611715
Zulkifli Mohamed <mr.putera@gmail.com>
2022-05-25 00:37:36 +0800

    Update Malaysian language

--- src/lang/ms/messages.php
+++ src/lang/ms/messages.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
+    'selectTime'    => 'Pilih masa',
     'empty_options' => 'Tiada rekod dijumpai.',
+    'loading'       => 'Loading...',
+    'searchHere'    => 'Cari',
     'datePicker'    => [
         'months'    => "['Januari', 'Februari', 'Mar', 'April', 'Mei', 'Jun', 'Julai', 'Ogos', 'September', 'Oktober', 'November', 'Disember']",
         'days'      => "['Ahd', 'Isn', 'Sel', 'Rab', 'Kha', 'Jum', 'Sab']",
