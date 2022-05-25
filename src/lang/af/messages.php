5120dd9f321a315b9b332a16cbe9553ccd3a8d4e
Ettienne Louw <ettienne.louwki@gmail.com>
2022-05-25 11:27:35 +0200

    Added afrikaans translations

---
+++ src/lang/af/messages.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'selectTime'    => 'Kies tyd',
+    'empty_options' => 'Leë Opsies',
+    'loading'       => 'Laai tans...',
+    'searchHere'    => 'Soek hier',
+    'datePicker'    => [
+        'months'    => "['Januarie', 'Februarie', 'Maart', 'April', 'Mei', 'Junie', 'Julie', 'Augustus', 'September', 'Oktober', 'November', 'Desember']",
+        'days'      => "['Son', 'Ma', 'Di', 'Wo', 'Do', 'Vry', 'Sat']",
+        'tomorrow'  => 'More',
+        'today'     => 'Vandag',
+        'yesterday' => 'Gister',
+    ],
+    'errors' => [
+        'title' => 'Daar was {errors} foute met jou indiening',
+    ],
+];
