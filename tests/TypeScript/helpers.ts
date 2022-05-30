fb5964ec0eea7d689d6880c2c2f27bdd6b182b6e
PH7-Jack <pedrolivertwd@gmail.com>
2022-05-30 03:29:30 -0300

    add testing helper to mock the alpine components

--- tests/TypeScript/helpers.ts
+++ tests/TypeScript/helpers.ts
@@ -1,3 +1,17 @@
+import { Component } from '@/components/alpine'
+
 export const sleep = (ms: number) => {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export const mockAlpineComponent = (component: Component): Component => {
+  component = Object.assign(component, {
+    $watch: jest.fn()
+  })
+
+  if (component.init) {
+    component.init()
+  }
+
+  return component
 }
