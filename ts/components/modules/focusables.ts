252b7a5ffcf81181af587c3b7105f409936660e1
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-10 05:49:29 -0300

    fix typing

--- ts/components/modules/focusables.ts
+++ ts/components/modules/focusables.ts
@@ -2,11 +2,11 @@ import { baseComponent, Component } from '../alpine'
 
 export interface Focusables extends Component {
   focusableSelector: string
-  getFocusables (): Element[]
-  getFirstFocusable (): Element | undefined
-  getLastFocusable (): Element | undefined
-  getNextFocusable (): Element
-  getPrevFocusable (): Element
+  getFocusables (): HTMLElement[]
+  getFirstFocusable (): HTMLElement | undefined
+  getLastFocusable (): HTMLElement | undefined
+  getNextFocusable (): HTMLElement
+  getPrevFocusable (): HTMLElement
   getNextFocusableIndex (): number
   getPrevFocusableIndex (): number
 }
@@ -16,21 +16,21 @@ export const focusables: Focusables = {
   focusableSelector: '',
   getFocusables () {
     return Array.from(this.$root.querySelectorAll(this.focusableSelector))
-      .filter(el => !el.hasAttribute('disabled'))
+      .filter(el => !el.hasAttribute('disabled')) as HTMLElement[]
   },
   getFirstFocusable () { return this.getFocusables().shift() },
   getLastFocusable () { return this.getFocusables().pop() },
   getNextFocusable () { return this.getFocusables()[this.getNextFocusableIndex()] || this.getFirstFocusable() },
   getPrevFocusable () { return this.getFocusables()[this.getPrevFocusableIndex()] || this.getLastFocusable() },
   getNextFocusableIndex () {
-    if (document.activeElement instanceof Element) {
+    if (document.activeElement instanceof HTMLElement) {
       return (this.getFocusables().indexOf(document.activeElement) + 1) % (this.getFocusables().length + 1)
     }
 
     return 0
   },
   getPrevFocusableIndex (): number {
-    if (document.activeElement instanceof Element) {
+    if (document.activeElement instanceof HTMLElement) {
       return Math.max(0, this.getFocusables().indexOf(document.activeElement)) - 1
     }
 
