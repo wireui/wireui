09bca6c493edd7b8d413a9347a4ac38e82ac8fb1
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-10 05:22:22 -0300

    prevent hidding the scrollbar when close modal
    when we have a child modal inside a parent modal,
    closing the child allows the body scroll again
    only the first modal changes the scroll now

--- ts/components/modal.ts
+++ ts/components/modal.ts
@@ -1,5 +1,7 @@
-import { baseComponent, Component, Entangle } from './alpine'
-import { focusables, Focusables } from './modules/focusables'
+import { ModalStore } from '@/alpine/store/modal'
+import { Component, Entangle } from '@/components/alpine'
+import { focusables, Focusables } from '@/components/modules/focusables'
+import uuid from '@/utils/uuid'
 
 export interface Options {
   model?: Entangle
@@ -8,30 +10,44 @@ export interface Options {
 
 export interface Modal extends Component, Focusables {
   show: Entangle
+  id: string
+  store: ModalStore
 
+  init (): void
   close (): void
   open (): void
+  toggleScroll (): void
 }
 
 export default (options: Options): Modal => ({
-  ...baseComponent,
   ...focusables,
   focusableSelector: 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])',
   show: options.model || options.show,
+  id: uuid(),
+  store: window.Alpine.store('wireui:modal'),
 
   init () {
     this.$watch('show', value => {
-      const elements = [...document.querySelectorAll('body, [main-container]')]
-
-      elements.forEach(el => {
-        value
-          ? el.classList.add('!overflow-hidden')
-          : el.classList.remove('!overflow-hidden')
-      })
-
       this.$el.dispatchEvent(new Event(value ? 'open' : 'close'))
     })
   },
-  close () { this.show = false },
-  open () { this.show = true }
+  close () {
+    this.show = false
+    this.toggleScroll()
+    this.store.remove(this.id)
+  },
+  open () {
+    this.show = true
+    this.store.setCurrent(this.id)
+    this.toggleScroll()
+  },
+  toggleScroll () {
+    if (!this.store.isCurrent(this.id)) return
+
+    const elements = [...document.querySelectorAll('body, [main-container]')]
+
+    this.show
+      ? elements.forEach(el => el.classList.add('!overflow-hidden'))
+      : elements.forEach(el => el.classList.remove('!overflow-hidden'))
+  }
 })
