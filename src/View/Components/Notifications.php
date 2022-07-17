fe901ec37fb6c49900b60e6d2d51a02c1c5bfe08
PH7-Jack <pedrolivertwd@gmail.com>
2022-07-16 23:44:01 -0300

    make the notifications component simple

--- src/View/Components/Notifications.php
+++ src/View/Components/Notifications.php
@@ -2,48 +2,37 @@
 
 namespace WireUi\View\Components;
 
-use Illuminate\Support\Str;
-use Illuminate\View\Component;
-
 class Notifications extends Component
 {
-    public string $zIndex;
-
-    public ?string $position;
+    public const TOP_LEFT      = 'top-left';
+    public const TOP_CENTER    = 'top-center';
+    public const TOP_RIGHT     = 'top-right';
+    public const BOTTOM_LEFT   = 'bottom-left';
+    public const BOTTOM_CENTER = 'bottom-center';
+    public const BOTTOM_RIGHT  = 'bottom-right';
 
     public function __construct(
-        string $zIndex = 'z-50',
-        ?string $position = 'top-right',
+        public string $zIndex = 'z-50',
+        public ?string $position = self::TOP_RIGHT,
     ) {
-        $this->zIndex = $zIndex;
-        $this->position = $this->setPosition($position);
-    }
-
-    public function setPosition($position): string
-    {
-        return Str::of('')
-            ->when($position == 'top-left', function ($string) {
-                return $string->append(' sm:items-start sm:justify-start');
-            })
-            ->when($position == 'top-center', function ($string) {
-                return $string->append(' sm:items-start sm:justify-center');
-            })
-            ->when($position == 'top-right', function ($string) {
-                return $string->append(' sm:items-start sm:justify-end');
-            })
-            ->when($position == 'bottom-left', function ($string) {
-                return $string->append(' sm:items-end sm:justify-start');
-            })
-            ->when($position == 'bottom-center', function ($string) {
-                return $string->append(' sm:items-end sm:justify-center');
-            })
-            ->when($position == 'bottom-right', function ($string) {
-                return $string->append(' sm:items-end sm:justify-end');
-            });
+        $this->zIndex   = $zIndex;
+        $this->position = $this->getPosition($position);
     }
 
     public function render()
     {
         return view('wireui::components.notifications');
+    }
+
+    public function getPosition(?string $position): string
+    {
+        return $this->classes([
+            'sm:items-start sm:justify-start'  => $position === self::TOP_LEFT,
+            'sm:items-start sm:justify-center' => $position === self::TOP_CENTER,
+            'sm:items-start sm:justify-end'    => $position === self::TOP_RIGHT,
+            'sm:items-end sm:justify-start'    => $position === self::BOTTOM_LEFT,
+            'sm:items-end sm:justify-center'   => $position === self::BOTTOM_CENTER,
+            'sm:items-end sm:justify-end'      => $position === self::BOTTOM_RIGHT,
+        ]);
     }
 }
