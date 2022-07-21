46afb489a4081e7792836edc8c95cbe5138eaa06
Ettienne Louw <ettienne.louwki@gmail.com>
2022-07-21 14:29:05 +0200

    Updates and fixes to avatars

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,6 +1,8 @@
 <span class="{{ $avatarClasses }}">
-    {{ $content ?? $slot }}
-    @if(!$content && $slot->isEmpty())
+
+    {{ $label ?? $src }}
+
+    @if (!$src && !$label)
         <svg class="w-full h-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
