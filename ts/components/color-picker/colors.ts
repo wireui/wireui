7afccd27ac299ff5689c478ee6baa94b7e79cf62
alabacakmurat <alabacakm@gmail.com>
2022-07-24 17:58:10 +0300

    Unnecessary import: tailwind-config

--- ts/components/color-picker/colors.ts
+++ ts/components/color-picker/colors.ts
@@ -1,34 +1,34 @@
-import resolveConfig from 'tailwindcss/resolveConfig'
-import { TailwindConfig } from 'tailwindcss/tailwind-config'
-import tailwindConfig from '@/../tailwind.config.js'
-import { Color } from '@/components/color-picker'
+import resolveConfig from "tailwindcss/resolveConfig";
+// import { TailwindConfig } from 'tailwindcss/tailwind-config'
+import tailwindConfig from "@/../tailwind.config.js";
+import { Color } from "@/components/color-picker";
 
 const excludeColors = [
-  'primary',
-  'secondary',
-  'positive',
-  'negative',
-  'warning',
-  'info'
-]
+  "primary",
+  "secondary",
+  "positive",
+  "negative",
+  "warning",
+  "info",
+];
 
 export const makeColors = (): Color[] => {
-  const config = resolveConfig(tailwindConfig as unknown as TailwindConfig)
-  const rawColors = config?.theme?.colors ?? {}
+  const config = resolveConfig(tailwindConfig);
+  const rawColors = config?.theme?.colors ?? {};
 
   const colors = Object.entries(rawColors).flatMap(([name, values]) => {
-    if (typeof values === 'string' || excludeColors.includes(name)) {
-      return []
+    if (typeof values === "string" || excludeColors.includes(name)) {
+      return [];
     }
 
     return Object.entries(values as object).map(([tonality, hex]) => ({
       name: `${name}-${tonality}`,
-      value: hex
-    }))
-  })
+      value: hex,
+    }));
+  });
 
-  colors.push({ name: 'White', value: '#fff' })
-  colors.push({ name: 'Black', value: '#000' })
+  colors.push({ name: "White", value: "#fff" });
+  colors.push({ name: "Black", value: "#000" });
 
-  return colors
-}
+  return colors;
+};
