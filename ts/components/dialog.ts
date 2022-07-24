519ab1c19c549a2dffe782894c73d174a46b93a2
alabacakmurat <alabacakm@gmail.com>
2022-07-24 17:57:53 +0300

    basePath implementation

--- ts/components/dialog.ts
+++ ts/components/dialog.ts
@@ -1,53 +1,53 @@
-import { parseConfirmation, parseDialog } from '../dialog/parses'
-import { Style } from '../dialog/options'
-import { Dialog, DialogOptions } from '../dialog'
-import { timer } from '../notifications/timer'
-import { Icon } from '../dialog/icons'
-import { Action, ButtonOptions } from '../dialog/actions'
+import { parseConfirmation, parseDialog } from "../dialog/parses";
+import { Style } from "../dialog/options";
+import { Dialog, DialogOptions } from "../dialog";
+import { timer } from "../notifications/timer";
+import { Icon } from "../dialog/icons";
+import { Action, ButtonOptions } from "../dialog/actions";
 
 export interface InitOptions {
-  id: string
+  id: string;
 }
 
 export interface Options extends DialogOptions {
-  accept?: Action
-  reject?: Action
+  accept?: Action;
+  reject?: Action;
 }
 
 export interface ParsedDialog extends Dialog {
-  accept?: Action
-  reject?: Action
+  accept?: Action;
+  reject?: Action;
 }
 
 export interface ParseOptions {
-  options: Options
-  componentId: string
+  options: Options;
+  componentId: string;
 }
 
 export interface DialogComponent {
-  [index: string]: any
+  [index: string]: any;
 
-  show: boolean
-  style: Style | null
-  dialog: ParsedDialog | null
+  show: boolean;
+  style: Style | null;
+  dialog: ParsedDialog | null;
 
-  init (): void
-  dismiss (): void
-  close (): void
-  open (): void
-  processDialog (options: ParsedDialog): void
-  showDialog (data: ParseOptions): void
-  confirmDialog (data: ParseOptions): void
-  fillIconBackground (icon: Icon | null): void
-  fillDialogIcon (icon: Icon | null): void
-  createButton (options: ButtonOptions, action: string): void
-  parseHtmlString (html: string): Element | null
-  startCloseTimeout (): void
-  accept (): void
-  reject (): void
-  handleEscape (): void
-  pauseTimeout (): void
-  resumeTimeout (): void
+  init(): void;
+  dismiss(): void;
+  close(): void;
+  open(): void;
+  processDialog(options: ParsedDialog): void;
+  showDialog(data: ParseOptions): void;
+  confirmDialog(data: ParseOptions): void;
+  fillIconBackground(icon: Icon | null): void;
+  fillDialogIcon(icon: Icon | null): void;
+  createButton(options: ButtonOptions, action: string): void;
+  parseHtmlString(html: string): Element | null;
+  startCloseTimeout(): void;
+  accept(): void;
+  reject(): void;
+  handleEscape(): void;
+  pauseTimeout(): void;
+  resumeTimeout(): void;
 }
 
 export default (options: InitOptions): DialogComponent => ({
@@ -55,148 +55,168 @@ export default (options: InitOptions): DialogComponent => ({
   style: null,
   dialog: null,
 
-  init () {
+  init() {
     this.$nextTick(() => {
-      window.Wireui.dispatchHook(`${options.id}:load`)
-    })
+      window.Wireui.dispatchHook(`${options.id}:load`);
+    });
   },
-  dismiss () {
-    this.close()
-    this.dialog?.onDismiss()
+  dismiss() {
+    this.close();
+    this.dialog?.onDismiss();
   },
-  close () {
-    this.show = false
-    this.dialog?.timer?.pause()
-    this.dialog?.onClose()
+  close() {
+    this.show = false;
+    this.dialog?.timer?.pause();
+    this.dialog?.onClose();
   },
-  open () {
-    this.show = true
+  open() {
+    this.show = true;
   },
-  processDialog (options) {
-    this.dialog = options
-    this.style = options.style
+  processDialog(options) {
+    this.dialog = options;
+    this.style = options.style;
 
-    if (this.$refs.title) { this.$refs.title.innerHTML = null }
-    if (this.$refs.description) { this.$refs.description.innerHTML = null }
+    if (this.$refs.title) {
+      this.$refs.title.innerHTML = null;
+    }
+    if (this.$refs.description) {
+      this.$refs.description.innerHTML = null;
+    }
 
     if (options.icon) {
-      this.fillIconBackground(options.icon)
-      this.fillDialogIcon(options.icon)
+      this.fillIconBackground(options.icon);
+      this.fillDialogIcon(options.icon);
     }
 
     if (options.accept) {
-      this.createButton(options.accept, 'accept')
+      this.createButton(options.accept, "accept");
     }
 
     if (options.reject) {
-      this.createButton(options.reject, 'reject')
+      this.createButton(options.reject, "reject");
     }
 
     if (options.close) {
-      this.createButton(options.close, 'close')
+      this.createButton(options.close, "close");
     }
 
     if (options.title) {
-      this.$refs.title.innerHTML = options.title
+      this.$refs.title.innerHTML = options.title;
     }
 
     if (options.description) {
-      this.$refs.description.innerHTML = options.description
+      this.$refs.description.innerHTML = options.description;
     }
 
-    this.$nextTick(() => this.open())
+    this.$nextTick(() => this.open());
 
     if (this.dialog?.timeout) {
-      this.startCloseTimeout()
+      this.startCloseTimeout();
     }
   },
-  showDialog ({ options, componentId }) {
-    this.processDialog(parseDialog(options, componentId))
+  showDialog({ options, componentId }) {
+    this.processDialog(parseDialog(options, componentId));
   },
-  confirmDialog ({ options, componentId }) {
-    this.processDialog(parseConfirmation(options, componentId))
+  confirmDialog({ options, componentId }) {
+    this.processDialog(parseConfirmation(options, componentId));
   },
-  fillIconBackground (icon) {
-    this.$refs.iconContainer.className = icon?.background ?? ''
+  fillIconBackground(icon) {
+    this.$refs.iconContainer.className = icon?.background ?? "";
   },
-  fillDialogIcon (icon) {
-    if (!icon?.name) return
+  fillDialogIcon(icon) {
+    if (!icon?.name) return;
 
-    const classes = ['w-10', 'h-10']
+    const classes = ["w-10", "h-10"];
 
     if (icon?.color) {
-      classes.push(...icon.color.split(' '))
+      classes.push(...icon.color.split(" "));
     }
 
-    if (this.style === 'inline') {
-      classes.push('sm:w-6', 'sm:h-6')
+    if (this.style === "inline") {
+      classes.push("sm:w-6", "sm:h-6");
     }
 
-    fetch(`/wireui/icons/${icon.style ?? 'outline'}/${icon.name}`, {
-      headers: {
-        'Accept': 'application/json'
+    fetch(
+      `${window.$wireui.config.basePath}/wireui/icons/${
+        icon.style ?? "outline"
+      }/${icon.name}`,
+      {
+        headers: {
+          Accept: "application/json",
+        },
       }
-    })
-      .then(response => response.text())
-      .then(text => {
-        const svg = new DOMParser().parseFromString(text, 'image/svg+xml').documentElement
-        svg.classList.add(...classes)
-        this.$refs.iconContainer.replaceChildren(svg)
-      })
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        const svg = new DOMParser().parseFromString(
+          text,
+          "image/svg+xml"
+        ).documentElement;
+        svg.classList.add(...classes);
+        this.$refs.iconContainer.replaceChildren(svg);
+      });
   },
-  createButton (options, action) {
-    const params = new URLSearchParams(options as string)
+  createButton(options, action) {
+    const params = new URLSearchParams(options as string);
 
-    params.delete('execute')
+    params.delete("execute");
 
-    fetch(`/wireui/button?${params}`, {
+    fetch(`${window.$wireui.config.basePath}/wireui/button?${params}`, {
       headers: {
-        'Accept': 'application/json'
-      }
+        Accept: "application/json",
+      },
     })
-      .then(response => response.text())
-      .then(html => {
-        const button = this.parseHtmlString(html)
+      .then((response) => response.text())
+      .then((html) => {
+        const button = this.parseHtmlString(html);
 
-        if (!button) return
+        if (!button) return;
 
-        button.setAttribute('x-on:click', action)
-        button.classList.add('w-full', 'dark:border-0', 'dark:hover:bg-secondary-700')
+        button.setAttribute("x-on:click", action);
+        button.classList.add(
+          "w-full",
+          "dark:border-0",
+          "dark:hover:bg-secondary-700"
+        );
 
-        this.$refs[action].replaceChildren(button)
-      })
+        this.$refs[action].replaceChildren(button);
+      });
   },
-  parseHtmlString (html) {
-    const div = document.createElement('div')
-    div.innerHTML = html
+  parseHtmlString(html) {
+    const div = document.createElement("div");
+    div.innerHTML = html;
 
-    return div.firstElementChild
+    return div.firstElementChild;
   },
-  startCloseTimeout () {
-    if (!this.dialog) return
+  startCloseTimeout() {
+    if (!this.dialog) return;
 
     this.dialog.timer = timer(
       this.dialog?.timeout ?? 0,
       () => {
-        this.close()
-        this.dialog?.onTimeout()
+        this.close();
+        this.dialog?.onTimeout();
       },
       (percentage) => {
-        this.$refs.progressbar.style.width = `${percentage}%`
+        this.$refs.progressbar.style.width = `${percentage}%`;
       }
-    )
+    );
   },
-  accept () {
-    this.close()
-    this.dialog?.accept?.execute()
+  accept() {
+    this.close();
+    this.dialog?.accept?.execute();
   },
-  reject () {
-    this.close()
-    this.dialog?.reject?.execute()
+  reject() {
+    this.close();
+    this.dialog?.reject?.execute();
   },
-  handleEscape () {
-    if (this.show) this.dismiss()
+  handleEscape() {
+    if (this.show) this.dismiss();
   },
-  pauseTimeout () { this.dialog?.timer?.pause() },
-  resumeTimeout () { this.dialog?.timer?.resume() }
-})
+  pauseTimeout() {
+    this.dialog?.timer?.pause();
+  },
+  resumeTimeout() {
+    this.dialog?.timer?.resume();
+  },
+});
--- ts/components/notifications.ts
+++ ts/components/notifications.ts
@@ -1,98 +1,109 @@
-import { parseConfirmation, parseNotification } from '../notifications/parses'
-import { timer } from '../notifications/timer'
-import uuid from '../utils/uuid'
+import { parseConfirmation, parseNotification } from "../notifications/parses";
+import { timer } from "../notifications/timer";
+import uuid from "../utils/uuid";
 
 export interface Notifications {
-  [index: string]: any
+  [index: string]: any;
 }
 
 export default (): Notifications => ({
   notifications: [],
 
-  init () {
+  init() {
     this.$nextTick(() => {
-      window.Wireui.dispatchHook('notifications:load')
-    })
+      window.Wireui.dispatchHook("notifications:load");
+    });
   },
-  proccessNotification (notification) {
-    notification.id = uuid()
+  proccessNotification(notification) {
+    notification.id = uuid();
 
     if (notification.timeout) {
       notification.timer = timer(
         notification.timeout,
         () => {
-          notification.onClose()
-          notification.onTimeout()
-          this.removeNotification(notification.id)
+          notification.onClose();
+          notification.onTimeout();
+          this.removeNotification(notification.id);
         },
         (percentage) => {
-          const progressBar = document.getElementById(`timeout.bar.${notification.id}`)
+          const progressBar = document.getElementById(
+            `timeout.bar.${notification.id}`
+          );
 
-          if (!progressBar) return
+          if (!progressBar) return;
 
-          progressBar.style.width = `${percentage}%`
+          progressBar.style.width = `${percentage}%`;
         }
-      )
+      );
     }
 
-    this.notifications.push(notification)
+    this.notifications.push(notification);
 
     if (notification.icon) {
       this.$nextTick(() => {
-        this.fillNotificationIcon(notification)
-      })
+        this.fillNotificationIcon(notification);
+      });
     }
   },
-  addNotification ({ options, componentId }) {
-    const notification = parseNotification(options, componentId)
+  addNotification({ options, componentId }) {
+    const notification = parseNotification(options, componentId);
 
-    this.proccessNotification(notification)
+    this.proccessNotification(notification);
   },
-  addConfirmNotification ({ options, componentId }) {
-    const notification = parseConfirmation(options, componentId)
+  addConfirmNotification({ options, componentId }) {
+    const notification = parseConfirmation(options, componentId);
 
-    this.proccessNotification(notification)
+    this.proccessNotification(notification);
   },
-  fillNotificationIcon (notification) {
-    const classes = `w-6 h-6 ${notification.icon.color}`.split(' ')
+  fillNotificationIcon(notification) {
+    const classes = `w-6 h-6 ${notification.icon.color}`.split(" ");
 
-    fetch(`/wireui/icons/outline/${notification.icon.name}`)
-      .then(response => response.text())
-      .then(text => {
-        const svg = new DOMParser().parseFromString(text, 'image/svg+xml').documentElement
+    fetch(
+      `${window.$wireui.config.basePath}/wireui/icons/outline/${notification.icon.name}`
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        const svg = new DOMParser().parseFromString(
+          text,
+          "image/svg+xml"
+        ).documentElement;
 
-        svg.classList.add(...classes)
+        svg.classList.add(...classes);
 
         document
           ?.getElementById(`notification.${notification.id}`)
-          ?.querySelector('.notification-icon')
-          ?.replaceChildren(svg)
-      })
+          ?.querySelector(".notification-icon")
+          ?.replaceChildren(svg);
+      });
   },
-  removeNotification (id) {
-    const index = this.notifications.findIndex(notification => notification.id === id)
+  removeNotification(id) {
+    const index = this.notifications.findIndex(
+      (notification) => notification.id === id
+    );
 
-    if (~index) { this.notifications.splice(index, 1) }
+    if (~index) {
+      this.notifications.splice(index, 1);
+    }
   },
-  closeNotification (notification) {
-    notification.onClose()
-    notification.onDismiss()
-    this.removeNotification(notification.id)
+  closeNotification(notification) {
+    notification.onClose();
+    notification.onDismiss();
+    this.removeNotification(notification.id);
   },
-  pauseNotification (notification) {
-    notification.timer?.pause()
+  pauseNotification(notification) {
+    notification.timer?.pause();
   },
-  resumeNotification (notification) {
-    notification.timer?.resume()
+  resumeNotification(notification) {
+    notification.timer?.resume();
   },
-  accept (notification) {
-    notification.onClose()
-    notification.accept.execute()
-    this.removeNotification(notification.id)
+  accept(notification) {
+    notification.onClose();
+    notification.accept.execute();
+    this.removeNotification(notification.id);
   },
-  reject (notification) {
-    notification.onClose()
-    notification.reject.execute()
-    this.removeNotification(notification.id)
-  }
-})
+  reject(notification) {
+    notification.onClose();
+    notification.reject.execute();
+    this.removeNotification(notification.id);
+  },
+});
