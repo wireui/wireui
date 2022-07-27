e54794b07065744516ac7e164682abf9b308f88c
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-27 17:44:26 -0300

    refactoring tests for alpine's magic functions

--- tests/TypeScript/alpine/magic/index.spec.ts
+++ tests/TypeScript/alpine/magic/index.spec.ts
@@ -1,5 +1,6 @@
 import '@/alpine/magic/index'
 import props from '@/alpine/magic/props'
+import tooltip from '@/alpine/magic/tooltip'
 import { AlpineMock } from '@tests/helpers'
 
 describe('Testing the index magic register', () => {
@@ -8,7 +9,8 @@ describe('Testing the index magic register', () => {
   it('should register all magic helpers', () => {
     document.dispatchEvent(new Event('alpine:init'))
 
-    expect(window.Alpine.magic).toBeCalledTimes(1)
+    expect(window.Alpine.magic).toBeCalledTimes(2)
     expect(window.Alpine.magic).toBeCalledWith('props', props)
+    expect(window.Alpine.magic).toBeCalledWith('tooltip', tooltip)
   })
 })
