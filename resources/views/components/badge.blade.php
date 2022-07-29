07653e344efbfce1d417ee3c0403de3327e8d005
Ettienne Louw <ettienne.louwki@gmail.com>
2022-07-29 19:41:29 +0200

    Update resources/views/components/badge.blade.php
    Co-authored-by: Pedro Oliveira <38913462+PH7-Jack@users.noreply.github.com>

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,12 +1,12 @@
 <span {{ $attributes->class($badgeClasses) }}>
-    @if($pulse)
-        <span class="flex absolute h-2 w-2">
+    @if ($pulse)
+        <span class="flex absolute h-2 w-2 mr-4">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 {{ $pulsePingColor }}"></span>
             <span class="relative inline-flex rounded-full h-2 w-2 {{ $pulseColor }}"></span>
         </span>
     @endif
 
-    <span @if($pulse) class="ml-4" @endif>{{ $title ?? $slot }}</span>
+    <span>{{ $title ?? $slot }}</span>
 
     @if($dismissible)
         {{ $dismissible }}
