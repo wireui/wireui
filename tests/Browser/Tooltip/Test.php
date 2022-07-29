212485e4c2adf17e6b263b3633e37c2cc2a2123e
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-29 16:04:59 -0300

    adding tests to the magic and directive methods of tooltip

--- tests/Browser/Tooltip/Test.php
+++ tests/Browser/Tooltip/Test.php
@@ -8,15 +8,52 @@ use Tests\Browser\BrowserTestCase;
 class Test extends BrowserTestCase
 {
     /** @test */
-    public function it_should_show_the_tooltip_when_hover_mouse_the_button()
+    public function it_should_show_the_tooltip_using_magic_method_when_click_in_the_button_and_it_will_disappear_in_2_seconds()
     {
         $this->browse(function (Browser $browser) {
             $this->visit($browser, Component::class)
                 ->assertSee('Click Here 1')
-                ->mouseover('@tooltip1')
-                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 1'))
+                ->click('@tooltip1')
+                ->waitUsing(3, 100, fn () => $browser->assertSee('Tooltip 1'))
+                ->waitUsing(3, 100, fn () => $browser->assertDontSee('Tooltip 1'));
+        });
+    }
+
+    /** @test */
+    public function it_should_show_the_tooltip_using_magic_method_when_click_in_the_button_and_it_will_disappear_in_500_milliseconds()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Click Here 2')
+                ->click('@tooltip2')
+                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 2'))
+                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 2'));
+        });
+    }
+
+    /** @test */
+    public function it_should_show_the_tooltip_using_directive_method_when_hover_mouse_the_button()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Click Here 3')
+                ->mouseover('@tooltip3')
+                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 3'))
                 ->moveMouse(600, 600)
-                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 1'));
+                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 3'));
+        });
+    }
+
+    /** @test */
+    public function it_should_show_the_tooltip_when_hover_mouse_the_button()
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Click Here 4')
+                ->mouseover('@tooltip4')
+                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 4'))
+                ->moveMouse(600, 600)
+                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 4'));
         });
     }
 
@@ -25,11 +62,11 @@ class Test extends BrowserTestCase
     {
         $this->browse(function (Browser $browser) {
             $this->visit($browser, Component::class)
-                ->assertSee('Click Here 2')
-                ->click('@tooltip2')
-                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 2'))
-                ->click('@tooltip2')
-                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 2'));
+                ->assertSee('Click Here 5')
+                ->click('@tooltip5')
+                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 5'))
+                ->click('@tooltip5')
+                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 5'));
         });
     }
 
@@ -38,22 +75,22 @@ class Test extends BrowserTestCase
     {
         $this->browse(function (Browser $browser) {
             $this->visit($browser, Component::class)
-                ->assertSee('Click Here 3')
-                ->click('@tooltip3')
-                ->waitUsing(3, 100, fn () => $browser->assertSee('Tooltip 3'))
-                ->waitUsing(3, 100, fn () => $browser->assertDontSee('Tooltip 3'));
+                ->assertSee('Click Here 6')
+                ->click('@tooltip6')
+                ->waitUsing(3, 100, fn () => $browser->assertSee('Tooltip 6'))
+                ->waitUsing(3, 100, fn () => $browser->assertDontSee('Tooltip 6'));
         });
     }
 
     /** @test */
-    public function it_should_show_the_tooltip_when_click_in_the_button_and_it_will_disappear_in_500_miliseconds()
+    public function it_should_show_the_tooltip_when_click_in_the_button_and_it_will_disappear_in_500_milliseconds()
     {
         $this->browse(function (Browser $browser) {
             $this->visit($browser, Component::class)
-                ->assertSee('Click Here 4')
-                ->click('@tooltip4')
-                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 4'))
-                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 4'));
+                ->assertSee('Click Here 7')
+                ->click('@tooltip7')
+                ->waitUsing(1, 100, fn () => $browser->assertSee('Tooltip 7'))
+                ->waitUsing(1, 100, fn () => $browser->assertDontSee('Tooltip 7'));
         });
     }
 }
--- tests/Browser/Tooltip/view.blade.php
+++ tests/Browser/Tooltip/view.blade.php
@@ -1,47 +1,68 @@
-<div class="mt-12 space-x-4 space-y-4">
+<div>
     <h1>Tooltip test</h1>
+
+    // test it_should_show_the_tooltip_using_magic_method_when_click_in_the_button_and_it_will_disappear_in_2_seconds
+    <div x-data="{}">
+        <x-button dusk="tooltip1" x-on:click="$tooltip('Tooltip 1')" green>
+            Click Here 1
+        </x-button>
+    </div>
+
+    // test it_should_show_the_tooltip_using_magic_method_when_click_in_the_button_and_it_will_disappear_in_500_milliseconds
+    <div x-data="{}">
+        <x-button dusk="tooltip2" x-on:click="$tooltip('Tooltip 2', 500)" green>
+            Click Here 2
+        </x-button>
+    </div>
+
+    // test it_should_show_the_tooltip_using_directive_method_when_hover_mouse_the_button
+    <div x-data="{}">
+        <x-button dusk="tooltip3" x-tooltip="Tooltip 3" green>
+            Click Here 3
+        </x-button>
+    </div>
 
     // test it_should_show_the_tooltip_when_hover_mouse_the_button
     <x-tooltip>
         <x-slot name="tooltip">
-            <strong>Tooltip 1</strong>
+            <strong>Tooltip 4</strong>
         </x-slot>
 
-        <x-button dusk="tooltip1" green>
-            Click Here 1
+        <x-button dusk="tooltip4" green>
+            Click Here 4
         </x-button>
     </x-tooltip>
 
     // test it_should_show_the_tooltip_when_click_in_the_button
     <x-tooltip trigger="click">
         <x-slot name="tooltip">
-            <strong>Tooltip 2</strong>
+            <strong>Tooltip 5</strong>
         </x-slot>
 
-        <x-button dusk="tooltip2" green>
-            Click Here 2
+        <x-button dusk="tooltip5" green>
+            Click Here 5
         </x-button>
     </x-tooltip>
 
     // test it_should_show_the_tooltip_when_click_in_the_button_and_it_will_disappear_in_2_seconds
     <x-tooltip trigger="manual">
         <x-slot name="tooltip">
-            <strong>Tooltip 3</strong>
+            <strong>Tooltip 6</strong>
         </x-slot>
 
-        <x-button dusk="tooltip3" x-on:click="dispatch" green>
-            Click Here 3
+        <x-button dusk="tooltip6" x-on:click="dispatch" green>
+            Click Here 6
         </x-button>
     </x-tooltip>
 
-    // test it_should_show_the_tooltip_when_click_in_the_button_and_it_will_disappear_in_500_miliseconds
+    // test it_should_show_the_tooltip_when_click_in_the_button_and_it_will_disappear_in_500_milliseconds
     <x-tooltip trigger="manual" timeout="500">
         <x-slot name="tooltip">
-            <strong>Tooltip 4</strong>
+            <strong>Tooltip 7</strong>
         </x-slot>
 
-        <x-button dusk="tooltip4" x-on:click="dispatch" green>
-            Click Here 4
+        <x-button dusk="tooltip7" x-on:click="dispatch" green>
+            Click Here 7
         </x-button>
     </x-tooltip>
 </div>
