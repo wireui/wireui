5b7fc43233c17c40432c642d131ee84582c6d856
PH7-Jack <pedrolivertwd@gmail.com>
2022-08-10 22:55:51 -0300

    fix modal and dialog height size in mobile

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,6 +1,6 @@
-@php $name = $name ?? $attributes->wire('model')->value(); @endphp
+@php($name = $name ?? $attributes->wire('model')->value())
 
-<div class="fixed inset-0 overflow-y-auto {{ $zIndex }}"
+<div class="fixed inset-0 flex items-end justify-center overflow-y-auto sm:pt-16 {{ $align }} {{ $spacing }} {{ $zIndex }}"
     x-data="wireui_modal({
         show: @toJs($show),
         @if ($attributes->wire('model')->value())
@@ -16,33 +16,29 @@
     x-cloak
     x-show="show"
     wireui-modal>
-    <div class="flex items-end {{ $align }} min-h-screen justify-center w-full
-                relative transform transition-all {{ $spacing }}"
-        style="min-height: -webkit-fill-available; min-height: fill-available;">
-        <div @class([
-                'fixed inset-0 bg-secondary-400 dark:bg-secondary-700 bg-opacity-60',
-                'dark:bg-opacity-60 transform transition-opacity',
-                $blur => (bool) $blur
-            ])
-            x-show="show"
-            x-on:click="close"
-            x-transition:enter="ease-out duration-300"
-            x-transition:enter-start="opacity-0"
-            x-transition:enter-end="opacity-100"
-            x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0">
-        </div>
+    <div @class([
+            'fixed inset-0 bg-secondary-400 dark:bg-secondary-700 bg-opacity-60',
+            'dark:bg-opacity-60 transform transition-opacity',
+            $blur => (bool) $blur
+        ])
+        x-show="show"
+        x-on:click="close"
+        x-transition:enter="ease-out duration-300"
+        x-transition:enter-start="opacity-0"
+        x-transition:enter-end="opacity-100"
+        x-transition:leave="ease-in duration-200"
+        x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0">
+    </div>
 
-        <div class="w-full {{ $maxWidth }} z-10"
-            x-show="show"
-            x-transition:enter="ease-out duration-300"
-            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-            {{ $slot }}
-        </div>
+    <div class="w-full {{ $maxWidth }} z-10"
+        x-show="show"
+        x-transition:enter="ease-out duration-300"
+        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+        x-transition:leave="ease-in duration-200"
+        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+        {{ $slot }}
     </div>
 </div>
