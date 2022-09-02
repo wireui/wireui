086489a995bd501c32e1cc45e1bcf9636d02e353
PH7-Jack <pedrolivertwd@gmail.com>
2022-09-01 22:59:06 -0300

    fix modal overflow

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,6 +1,6 @@
 @php($name = $name ?? $attributes->wire('model')->value())
 
-<div class="fixed inset-0 flex items-end justify-center overflow-y-auto sm:pt-16 {{ $align }} {{ $spacing }} {{ $zIndex }}"
+<div class="fixed inset-0 overflow-y-auto {{ $spacing }} {{ $zIndex }}"
     x-data="wireui_modal({
         show: @toJs($show),
         @if ($attributes->wire('model')->value())
@@ -11,7 +11,9 @@
     x-on:keydown.tab.prevent="handleTab"
     x-on:keydown.shift.tab.prevent="handleShiftTab"
     x-on:open-wireui-modal:{{ Str::kebab($name) }}.window="open"
-    {{ $attributes->whereStartsWith(['x-on:', '@']) }}
+    {{ $attributes
+        ->whereDoesntStartWith('wire:model')
+        ->whereStartsWith(['x-on:', '@', 'wire:']) }}
     style="display: none"
     x-cloak
     x-show="show"
@@ -31,8 +33,9 @@
         x-transition:leave-end="opacity-0">
     </div>
 
-    <div class="w-full {{ $maxWidth }} z-10"
+    <div class="w-full min-h-full transform flex items-end justify-center mx-auto {{ $align }} {{ $maxWidth }}"
         x-show="show"
+        x-on:click.self="close"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
