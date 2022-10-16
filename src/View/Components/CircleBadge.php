9febf067e64c91887af7a0d772871001c18fc497
João Pedro Lopes <joaopalopes24@gmail.com>
2022-10-16 00:50:44 -0300

    updating icon sizes in badge circle

--- src/View/Components/CircleBadge.php
+++ src/View/Components/CircleBadge.php
@@ -45,8 +45,8 @@ class CircleBadge extends Badge
     {
         return [
             self::DEFAULT => 'w-6 h-6',
-            'md'          => 'w-8 h-8',
-            'lg'          => 'w-10 h-10',
+            'md'          => 'w-7 h-7',
+            'lg'          => 'w-8 h-8',
         ];
     }
 
