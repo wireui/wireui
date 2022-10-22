919f67b8eba71eb96522426be0772df4c5bfe577
João Pedro Lopes <joaopalopes24@gmail.com>
2022-10-22 20:25:07 -0300

    create component

---
+++ src/View/Components/Inputs/SliderInput.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace WireUi\View\Components\Inputs;
+
+use Exception;
+use Illuminate\Support\{Str, Stringable};
+use Illuminate\View\ComponentAttributeBag;
+use WireUi\View\Components\FormComponent;
+
+class SliderInput extends FormComponent
+{
+    protected string $size = 'sm';
+
+    public function __construct(
+        public bool $md = false,
+        public bool $lg = false,
+        public bool $range = false,
+        public bool $errorless = false,
+        public bool $showStops = false,
+        public bool $hideTooltip = false,
+        public ?string $label = null,
+        public ?string $hint = null,
+        public ?string $cornerHint = null,
+    ) {
+        $this->size = $this->md ? 'md' : $this->size;
+        $this->size = $this->lg ? 'lg' : $this->size;
+    }
+
+    protected function getView(): string
+    {
+        return $this->range ? 'wireui::components.inputs.slider-range' : 'wireui::components.inputs.slider';
+    }
+
+    // private function validateConfig(): void
+    // {
+    //     if ($this->options->isNotEmpty() && $this->asyncData['api']) {
+    //         throw new Exception('The {async-data} attribute cannot be used with {options} attribute.');
+    //     }
+    // }
+
+    public function formatDataSliderRange(ComponentAttributeBag $attributes, string $type)
+    {
+        $copy = clone $attributes;
+
+        $model = $copy->wire('model');
+
+        if (is_string($model->directive)) {
+            $copy->offsetSet($model->directive, "{$model->value}_{$type}");
+        }
+
+        if ($copy->has('name')) {
+            $copy->offsetSet('name', "{$copy->get('name')}_{$type}");
+        }
+
+        return $copy;
+    }
+
+    public function formatDataSlider(ComponentAttributeBag $attributes): array
+    {
+        return [
+            'type' => 'number',
+            'min'  => $attributes->has('min') ? $attributes->get('min') : 0,
+            'max'  => $attributes->has('max') ? $attributes->get('max') : 100,
+            'step' => $attributes->has('step') ? $attributes->get('step') : 1,
+        ];
+    }
+
+    public function getSliderClasses(?bool $disabled = null): string
+    {
+        return Str::of('relative w-full align-middle bg-secondary-200 rounded')
+            ->when(!$disabled, fn (Stringable $stringable) => $stringable->append(' cursor-pointer'))
+            ->when($this->size === 'sm', fn (Stringable $stringable) => $stringable->append(' my-4 h-1.5'))
+            ->when($this->size === 'md', fn (Stringable $stringable) => $stringable->append(' my-4.5 h-2'))
+            ->when($this->size === 'lg', fn (Stringable $stringable) => $stringable->append(' my-5 h-2.5'));
+    }
+
+    public function getBarClasses(?bool $disabled = null, bool $hasError = false): string
+    {
+        return Str::of('absolute rounded-l')
+            ->when($disabled, fn (Stringable $stringable) => $stringable->append(' bg-secondary-500 opacity-60'))
+            ->when(!$disabled && $hasError, fn (Stringable $stringable) => $stringable->append(' bg-negative-600'))
+            ->when(!$disabled && !$hasError, fn (Stringable $stringable) => $stringable->append(' bg-primary-600'))
+            ->when($this->size === 'sm', fn (Stringable $stringable) => $stringable->append(' h-1.5'))
+            ->when($this->size === 'md', fn (Stringable $stringable) => $stringable->append(' h-2'))
+            ->when($this->size === 'lg', fn (Stringable $stringable) => $stringable->append(' h-2.5'));
+    }
+
+    public function getButtonGridClasses(): string
+    {
+        return <<<EOT
+            absolute leading-normal text-center -translate-x-1/2
+            bg-transparent select-none w-9 h-9 -top-4
+        EOT;
+    }
+
+    public function getButtonClasses(?bool $disabled = null): string
+    {
+        $enabledClasses = 'border-2 bg-white cursor-grab hover:scale-120';
+
+        $disabledClasses = 'border-2 bg-white ring-transparent cursor-not-allowed';
+
+        return $disabled ? $disabledClasses : $enabledClasses;
+    }
+
+    public function buttonError(?bool $disabled = null, bool $hasError = false): string
+    {
+        return $disabled ? 'secondary' : ($hasError ? 'negative' : 'primary');
+    }
+
+    public function buttonSizes(): string
+    {
+        return match ($this->size) {
+            'sm' => '2xs',
+            'md' => 'xs',
+            'lg' => 'sm',
+        };
+    }
+}
