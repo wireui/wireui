83224cb81f4644d7cdbafc78f8ce741c158e00a9
João Pedro Lopes <joaopalopes24@gmail.com>
2022-10-23 13:16:22 -0300

    adding a basic test layer to the input slider

--- tests/Browser/SliderInput/Test.php
+++ tests/Browser/SliderInput/Test.php
@@ -2,9 +2,95 @@
 
 namespace Tests\Browser\SliderInput;
 
+use Laravel\Dusk\Browser;
 use Tests\Browser\BrowserTestCase;
 
 class Test extends BrowserTestCase
 {
-    //
+    /** @test */
+    public function it_should_1(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Test 1')
+                ->assertSee('corner-hint test 1')
+                ->assertSee('hint-test-1')
+                ->assertValue('input[name="test1"]', '')
+                ->click('input[name="test1"] + div')
+                ->assertValue('input[name="test1"]', '50')
+                ->assertSee('50')
+                ->clickAndHold('input[name="test1"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(260, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test1"]', '76')
+                ->clickAndHold('input[name="test1"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(-512, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test1"]', '25');
+        });
+    }
+
+    /** @test */
+    public function it_should_2(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Test 2')
+                ->assertSee('corner-hint test 2')
+                ->assertSee('hint-test-2')
+                ->assertValue('input[name="test2"]', '')
+                ->click('input[name="test2"] + div')
+                ->assertValue('input[name="test2"]', '5.5')
+                ->assertSee('5.5')
+                ->clickAndHold('input[name="test2"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(260, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test2"]', '5.8')
+                ->clickAndHold('input[name="test2"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(-512, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test2"]', '5.3');
+        });
+    }
+
+    /** @test */
+    public function it_should_3(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Test 3')
+                ->assertValue('input[name="test3"]', '')
+                ->click('input[name="test3"] + div')
+                ->assertValue('input[name="test3"]', '50')
+                ->assertDontSee('50')
+                ->clickAndHold('input[name="test3"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(260, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test3"]', '75')
+                ->clickAndHold('input[name="test3"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(-450, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test3"]', '30');
+        });
+    }
+
+    /** @test */
+    public function it_should_4(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $this->visit($browser, Component::class)
+                ->assertSee('Test 4')
+                ->assertValue('input[name="test4"]', '40')
+                ->click('input[name="test4"] + div')
+                ->assertValue('input[name="test4"]', '60')
+                ->clickAndHold('input[name="test4"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(160, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test4"]', '80')
+                ->clickAndHold('input[name="test4"] + div > div:nth-child(2) > div > button')
+                ->moveMouse(-660, 0)
+                ->releaseMouse()
+                ->assertValue('input[name="test4"]', '20');
+        });
+    }
 }
--- tests/Browser/SliderInput/view.blade.php
+++ tests/Browser/SliderInput/view.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- View --}}
+    <h1>Slider Input test</h1>
+
+    // test it_should_1
+    <x-inputs.slider name="test1" label="Test 1" corner-hint="corner-hint test 1" hint="hint-test-1" />
+
+    // test it_should_2
+    <x-inputs.slider name="test2" label="Test 2" corner-hint="corner-hint test 2" hint="hint-test-2" min="5" max="6" step="0.1" />
+
+    // test it_should_3
+    <x-inputs.slider md name="test3" label="Test 3" step="5" hide-tooltip />
+
+    // test it_should_4
+    <x-inputs.slider lg name="test4" label="Test 4" step="20" show-stops />
 </div>
