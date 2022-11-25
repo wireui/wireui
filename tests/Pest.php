951d5f3479979d6c5d4cfc2b1d9c3f6cc8ba9ba4
PH7-Jack <pedrolivertwd@gmail.com>
2022-11-24 22:32:05 -0300

    add render expection helper

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,4 +1,14 @@
 <?php
 
+use Pest\Expectation;
+
 uses(Tests\Browser\BrowserTestCase::class)->in('Browser');
 uses(Tests\Unit\TestCase::class)->in('Unit');
+
+expect()->extend('render', function (array $data = []): Expectation {
+    /** @var Expectation $this */
+
+    $testView = test()->blade($this->value, $data);
+
+    return expect((string) $testView);
+});
