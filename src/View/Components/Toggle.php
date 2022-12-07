abaebe69a19bddb8f393c8b3a305adf3e98fb6c4
João Pedro Lopes <joaopalopes24@gmail.com>
2022-12-07 15:14:19 -0300

    adjust classes in toggle component

--- src/View/Components/Toggle.php
+++ src/View/Components/Toggle.php
@@ -19,33 +19,42 @@ class Toggle extends Checkbox
             'h-6 w-10' => $this->size === 'lg',
         ]);
 
-        return Arr::toCssClasses([
-            'block rounded-full cursor-pointer transition ease-in-out duration-100',
-            "peer-focus:ring-2 peer-focus:ring-offset-2 {$size}",
-            'group-focus:ring-2 group-focus:ring-offset-2',
-            'bg-secondary-200 peer-checked:bg-primary-600 peer-focus:ring-primary-600'     => !$hasError,
-            'group-focus:ring-primary-600 dark:group-focus:ring-secondary-600'             => !$hasError,
-            'dark:peer-focus:ring-secondary-600 dark:peer-focus:ring-offset-secondary-800' => !$hasError,
-            'dark:bg-secondary-600 dark:peer-checked:bg-secondary-700'                     => !$hasError,
-            'bg-negative-600 peer-focus:ring-negative-600 dark:bg-negative-700'            => $hasError,
-            'group-focus:ring-negative-600 dark:group-focus:ring-negative-700'             => $hasError,
-            'dark:peer-focus:ring-negative-700 dark:peer-focus:ring-offset-secondary-800'  => $hasError,
-        ]);
+        $default = <<<EOT
+            block rounded-full cursor-pointer transition ease-in-out duration-100
+            peer-focus:ring-2 peer-focus:ring-offset-2
+            group-focus:ring-2 group-focus:ring-offset-2
+        EOT;
+
+        $withError = <<<EOT
+            bg-negative-600 peer-focus:ring-negative-600 dark:bg-negative-700
+            group-focus:ring-negative-600 dark:group-focus:ring-negative-700
+            dark:peer-focus:ring-negative-700 dark:peer-focus:ring-offset-secondary-800
+        EOT;
+
+        $withoutError = <<<EOT
+            bg-secondary-200 peer-checked:bg-primary-600 peer-focus:ring-primary-600
+            group-focus:ring-primary-600 dark:group-focus:ring-secondary-600
+            dark:peer-focus:ring-secondary-600 dark:peer-focus:ring-offset-secondary-800
+            dark:bg-secondary-600 dark:peer-checked:bg-secondary-700
+        EOT;
+
+        return Arr::toCssClasses([$size, $default, $withError => $hasError, $withoutError => !$hasError]);
     }
 
     public function circleClasses(): string
     {
-        $classes = Arr::toCssClasses([
-            'checked:translate-x-3 w-3 h-3'                => $this->size === 'sm',
-            'checked:translate-x-3.5 left-0.5 w-3.5 h-3.5' => $this->size === 'md',
-            'checked:translate-x-4 left-0.5 w-4 h-4'       => $this->size === 'lg',
-        ]);
-
-        return <<<EOT
-            absolute mx-0.5 my-auto inset-y-0 {$classes} rounded-full border-0 appearance-none
+        $classes = <<<EOT
+            absolute mx-0.5 my-auto inset-y-0 rounded-full border-0 appearance-none
             translate-x-0 transform transition ease-in-out duration-200 cursor-pointer shadow
             checked:bg-none peer focus:ring-0 focus:ring-offset-0 focus:outline-none bg-white
             checked:text-white dark:bg-secondary-200
         EOT;
+
+        return Arr::toCssClasses([
+            $classes,
+            'checked:translate-x-3 w-3 h-3'                => $this->size === 'sm',
+            'checked:translate-x-3.5 left-0.5 w-3.5 h-3.5' => $this->size === 'md',
+            'checked:translate-x-4 left-0.5 w-4 h-4'       => $this->size === 'lg',
+        ]);
     }
 }
