50d57404c56e54b2bb8ac82b62630e465fe8af76
João Pedro Lopes <joaopalopes24@gmail.com>
2022-12-08 08:22:53 -0300

    changing merge function to class function

--- src/resources/views/components/dropdown/header.blade.php
+++ src/resources/views/components/dropdown/header.blade.php
@@ -1,5 +1,5 @@
 <div class="@if($separator) border-t border-secondary-200 dark:border-secondary-600 @endif">
-    <h6 {{ $attributes->merge(['class' => $classes]) }}>
+    <h6 {{ $attributes->class($classes) }}>
         {{ $label }}
     </h6>
 
