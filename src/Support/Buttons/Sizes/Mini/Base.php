65af9e17ceb4225a1bb04955de219ef3346dcf27
PH7-Jack <pedrolivertwd@gmail.com>
2022-12-12 21:40:02 -0300

    add custom font-size to mini buttons

--- src/Support/Buttons/Sizes/Mini/Base.php
+++ src/Support/Buttons/Sizes/Mini/Base.php
@@ -14,13 +14,13 @@ class Base extends SizePack
     public function all(): array
     {
         return [
-            '2xs' => 'w-5 h-5',
-            'xs'  => 'w-7 h-7',
-            'sm'  => 'w-8 h-8',
-            'md'  => 'w-9 h-9',
-            'lg'  => 'w-10 h-10',
-            'xl'  => 'w-12 h-12',
-            '2xl' => 'w-14 h-14',
+            '2xs' => 'text-2xs w-5 h-5',
+            'xs'  => 'text-xs w-7 h-7',
+            'sm'  => 'text-sm w-8 h-8',
+            'md'  => 'text-sm w-9 h-9',
+            'lg'  => 'text-base w-10 h-10',
+            'xl'  => 'text-lg w-12 h-12',
+            '2xl' => 'text-xl w-14 h-14',
         ];
     }
 }
