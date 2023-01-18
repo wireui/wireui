7e5ab2fc512c1f859e4fd96f820701232ac154db
João Pedro Lopes <joaopalopes24@gmail.com>
2023-01-18 14:58:23 -0300

    adding tests to alert, card and errors components

--- tests/Unit/View/Components/ErrorsTest.php
+++ tests/Unit/View/Components/ErrorsTest.php
@@ -1,5 +1,40 @@
 <?php
 
-it('should', function () {
-    expect(true)->toBeTrue();
+use Illuminate\Support\{MessageBag, ViewErrorBag};
+use WireUi\View\Components\Errors;
+
+it('should 1', function () {
+    $errors = new Errors();
+
+    expect($errors->title)->toBe(trans('wireui::messages.errors.title'));
+});
+
+it('should 2', function () {
+    $errors = new Errors(only: 'name1|name2|name3');
+
+    expect($errors->only->toArray())->toBe(['name1', 'name2', 'name3']);
+
+    $errors = new Errors(only: ['name1', 'name2', 'name3']);
+
+    expect($errors->only->toArray())->toBe(['name1', 'name2', 'name3']);
+});
+
+it('should 3', function () {
+    $errors = new Errors();
+
+    $errorBag = new ViewErrorBag();
+
+    $messages = new MessageBag([
+        'error1' => 'message1',
+        'error2' => 'message2',
+        'error3' => 'message3',
+    ]);
+
+    $errorBag->put('default', $messages);
+
+    expect($errors->getErrorMessages($errorBag)->toArray())->toBe([
+        'error1' => ['message1'],
+        'error2' => ['message2'],
+        'error3' => ['message3'],
+    ]);
 });
