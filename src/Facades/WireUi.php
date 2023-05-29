e1a6075214d7eed6b2cc4c8a1c055b34292764cc
João Pedro Lopes <joaopalopes24@gmail.com>
2023-05-29 14:58:24 -0300

    fix(support): fixing class import in wireui facade

--- src/Facades/WireUi.php
+++ src/Facades/WireUi.php
@@ -3,8 +3,7 @@
 namespace WireUi\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use WireUi\Support\{BladeDirectives, ComponentResolver};
-use WireUi\WireUiSupport;
+use WireUi\Support\{BladeDirectives, ComponentResolver, WireUiSupport};
 
 /**
  * @method static string component(string $name)
