d3bf49d2aaae4722056857d83c4551f2a57ad14c
Jeffrey Cobb <jeffrey.cobb@insightlpr.com>
2023-06-06 12:25:58 -0400

    Adds global method $closeModal

--- ts/global/modal.ts
+++ ts/global/modal.ts
@@ -3,3 +3,6 @@ import kebabCase from 'lodash.kebabcase'
 window.$openModal = name => {
   return window.dispatchEvent(new Event(`open-wireui-modal:${kebabCase(name)}`))
 }
+window.$closeModal = name => {
+  return window.dispatchEvent(new Event(`close-wireui-modal:${kebabCase(name)}`))
+}
