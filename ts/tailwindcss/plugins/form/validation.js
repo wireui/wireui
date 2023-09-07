1906032ae8e7405e3fee42e871d68545c0122e7b
PH7-Jack <pedrolivertwd@gmail.com>
2023-09-06 22:40:37 -0300

    simplify css selectors

--- ts/tailwindcss/plugins/form/validation.js
+++ ts/tailwindcss/plugins/form/validation.js
@@ -7,12 +7,9 @@ module.exports = plugin(function ({ addVariant }) {
       `[group-${alias}] &`,
       `form[with-validation-colors] &:${modifier}`,
       `form[with-validation-colors] > div[form-wrapper] > &:has(input:${modifier})`,
-      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) ~ &[name="form.wrapper.description"]`,
-      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) &[name="form.wrapper.container.prefix"]`,
-      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) &[name="form.wrapper.container.suffix"]`,
-      `form[with-validation-colors] > div[form-wrapper] > div[name="form.wrapper.header"]:has(+ label[name="form.wrapper.container"] > input:${modifier}) > &`,
-      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) > div[name="form.wrapper.container.prepend"] &:is(button, a)`,
-      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) > div[name="form.wrapper.container.append"] &:is(button, a)`
+      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) ~ &`,
+      `form[with-validation-colors] > div[form-wrapper] > label[name="form.wrapper.container"]:has(input:${modifier}) &`,
+      `form[with-validation-colors] > div[form-wrapper] > div[name="form.wrapper.header"]:has(+ label[name="form.wrapper.container"] > input:${modifier}) > &`
     ]
   }
 
