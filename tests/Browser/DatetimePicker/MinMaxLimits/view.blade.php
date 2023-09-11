92844cfc1c7e4e19d0faba90ee12bd780282bd0c
João Pedro Lopes <joaopalopes24@gmail.com>
2023-09-10 23:41:15 -0300

    test(datetime): adjust datetime view test

--- tests/Browser/DatetimePicker/MinMaxLimits/view.blade.php
+++ tests/Browser/DatetimePicker/MinMaxLimits/view.blade.php
@@ -15,7 +15,7 @@ mount(function () {
     <span dusk="value">{{ $model }}</span>
 
     <x-datetime-picker
-        wire:model="model"
+        wire:model.live="model"
         without-timezone
         :min="$date->copy()->subDays(7)->setHour(12)->toISOString()"
         :max="$date->copy()->addDays(7)->setHour(15)->toISOString()"
