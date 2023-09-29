b710e538145de2334ecde69564e78a26adb4c6ab
PH7-Jack <pedrolivertwd@gmail.com>
2023-09-29 13:38:07 -0300

    implement caching on x-props

--- ts/alpine/magic/props.ts
+++ ts/alpine/magic/props.ts
@@ -1,18 +1,36 @@
 import { Component } from '@/components/alpine'
 
 export const props = function (el: HTMLElement): object {
-  const $root = window.Alpine.evaluate(el, '$root')
-  const attribute = $root?.getAttribute('x-props')
+  const $root = el.closest('[x-data]')
+  const expression = $root?.getAttribute('x-props')
 
-  if (!attribute) return {}
+  if (!expression || !$root) return {}
 
-  return window.Alpine.evaluate($root, attribute)
+  const cacheKey = `x-props:${expression}`
+
+  const cache = window.Wireui.cache[cacheKey]
+
+  if (cache) {
+    return cache
+  }
+
+  const evaluated = window.Alpine.evaluate($root, expression)
+
+  window.Wireui.cache[cacheKey] = evaluated
+
+  return evaluated
 }
 
 export function watchProps (component: Component, callback: CallableFunction): void {
-  const observer = new MutationObserver(
-    (mutations) => callback(mutations)
-  )
+  const observer = new MutationObserver(mutations => {
+    const wasChanged = mutations.some(
+      mutation => mutation.attributeName === 'x-props'
+    )
+
+    if (wasChanged) {
+      callback(mutations)
+    }
+  })
 
   observer.observe(component.$root, { attributes: true })
 
