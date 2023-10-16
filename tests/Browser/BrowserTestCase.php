4e3bcfb2214e183cd7617ef0385ffbefdbbb9c75
PH7-Jack <pedrolivertwd@gmail.com>
2023-10-15 23:51:43 -0300

    add livewire dusk support

--- tests/Browser/BrowserTestCase.php
+++ tests/Browser/BrowserTestCase.php
@@ -5,18 +5,19 @@ namespace Tests\Browser;
 use Illuminate\Support\Facades\{Artisan, File};
 use Laravel\Dusk\Browser;
 use Livewire\LivewireServiceProvider;
-use Livewire\Volt\{Volt, VoltServiceProvider};
 use Orchestra\Testbench\Dusk\{Options, TestCase};
 use Tests\Browser\Macros\DuskBrowserMacros;
 use WireUi\Heroicons\HeroiconsServiceProvider;
 use WireUi\ServiceProvider;
+
+use function Livewire\trigger;
 
 /** @link https://github.com/livewire/livewire/blob/main/tests/BrowserTestCase.php */
 class BrowserTestCase extends TestCase
 {
     use BrowserFunctions;
 
-    protected function setUp(): void
+    public function setUp(): void
     {
         if (isset($_SERVER['CI'])) {
             Options::withoutUI();
@@ -36,12 +37,14 @@ class BrowserTestCase extends TestCase
 
         parent::setUp();
 
-        $this->tweakApplication(fn () => Volt::mount(__DIR__));
+        trigger('browser.testCase.setUp', $this);
     }
 
-    protected function tearDown(): void
+    public function tearDown(): void
     {
         $this->removeApplicationTweaks();
+
+        trigger('browser.testCase.tearDown', $this);
 
         parent::tearDown();
     }
@@ -50,18 +53,26 @@ class BrowserTestCase extends TestCase
     {
         Artisan::call('view:clear');
 
+        File::deleteDirectory($this->tmpBladePath());
         File::deleteDirectory($this->livewireViewsPath());
         File::deleteDirectory($this->livewireClassesPath());
         File::deleteDirectory($this->livewireTestsPath());
         File::delete(app()->bootstrapPath('cache/livewire-components.php'));
+
+        File::ensureDirectoryExists($this->tmpBladePath());
+    }
+
+    public static function tweakApplicationHook()
+    {
+        return function () {
+        };
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            VoltServiceProvider::class,
-            ServiceProvider::class,
             LivewireServiceProvider::class,
+            ServiceProvider::class,
             HeroiconsServiceProvider::class,
         ];
     }
@@ -73,6 +84,8 @@ class BrowserTestCase extends TestCase
         });
 
         tap($app['config'], function ($config) {
+            $config->set('app.env', 'testing');
+
             $config->set('app.debug', true);
 
             $config->set('view.paths', [__DIR__ . '/views', resource_path('views')]);
@@ -89,17 +102,22 @@ class BrowserTestCase extends TestCase
         });
     }
 
-    protected function livewireClassesPath($path = '')
+    public static function tmpPath(string $path): string
+    {
+        return __DIR__ . "/tmp/{$path}";
+    }
+
+    protected function livewireClassesPath($path = ''): string
     {
         return app_path('Livewire' . ($path ? '/' . $path : ''));
     }
 
-    protected function livewireViewsPath($path = '')
+    protected function livewireViewsPath($path = ''): string
     {
         return resource_path('views') . '/livewire' . ($path ? '/' . $path : '');
     }
 
-    protected function livewireTestsPath($path = '')
+    protected function livewireTestsPath($path = ''): string
     {
         return base_path('tests/Feature/Livewire' . ($path ? '/' . $path : ''));
     }
