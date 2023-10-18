d3d35d1687ff035fb53b123eacc5f22e4065e171
Joseph Bassey <joemires20@yahoo.com>
2023-10-18 08:44:53 +0100

    Fix unless closing bug in modal persistence check
    Signed-off-by: Joseph Bassey <joemires20@yahoo.com>

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -26,7 +26,7 @@
         x-show="show"
         @unless($persistent)
             x-on:click="close"
-        @unless
+        @endunless
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100"
@@ -39,7 +39,7 @@
         x-show="show"
         @unless($persistent)
             x-on:click.self="close"
-        @unless
+        @endunless
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
