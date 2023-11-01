459529079718ef5e37fbd2cc000c043f69c08bdd
PH7-Jack <pedrolivertwd@gmail.com>
2023-10-31 22:54:31 -0300

    refactor toggle

--- src/View/Components/Toggle.php
+++ src/View/Components/Toggle.php
@@ -3,7 +3,6 @@
 namespace WireUi\View\Components;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Arr;
 use WireUi\Traits\Components\IsFormComponent;
 use WireUi\Traits\Components\{HasSetupColor, HasSetupRounded, HasSetupSize};
 
@@ -22,36 +21,6 @@ class Toggle extends WireUiComponent
         'right-icon'  => null,
         'description' => null,
     ];
-
-    public function backgroundClasses(bool $hasError): string
-    {
-        $default = Arr::toCssClasses([
-            'block rounded-full cursor-pointer transition ease-in-out duration-100',
-            'peer-focus:ring-2 peer-focus:ring-offset-2',
-            'group-focus:ring-2 group-focus:ring-offset-2',
-        ]);
-
-        return Arr::toCssClasses([
-            data_get($this->sizeClasses, 'background', ''),
-            $this->colorClasses,
-            $default,
-        ]);
-    }
-
-    public function circleClasses(): string
-    {
-        $default = Arr::toCssClasses([
-            'translate-x-0 transform transition ease-in-out duration-200 cursor-pointer shadow',
-            'checked:bg-none peer focus:ring-0 focus:ring-offset-0 focus:outline-none bg-white',
-            'absolute mx-0.5 my-auto inset-y-0 rounded-full border-0 appearance-none',
-            'checked:text-white dark:bg-secondary-200',
-        ]);
-
-        return Arr::toCssClasses([
-            data_get($this->sizeClasses, 'circle', ''),
-            $default,
-        ]);
-    }
 
     protected function blade(): View
     {
--- src/resources/views/components/toggle.blade.php
+++ src/resources/views/components/toggle.blade.php
@@ -1,42 +1,38 @@
-<div>
-    <div class="flex items-center">
-        @if ($leftLabel)
-            <x-dynamic-component
-                :component="WireUi::component('label')"
-                :for="$id"
-                class="mr-2"
-                :label="$leftLabel"
-                :has-error="$errors->has($name)"
-            />
-        @endif
+<x-wrapper.switcher
+    :attributes="$attributes->only(['wire:key'])"
+    :data="$wrapperData"
+>
+    @include('wireui::components.wrapper.slots')
 
-        <label for="{{ $id }}" tabindex="-1" class="relative flex items-center select-none group">
-            <input {{ $attributes->merge([
-                    'name'  => $name,
-                    'id'    => $id,
-                    'class' => $circleClasses(),
-                ]) }}
-                type="checkbox"
-            />
-
-            <div class="{{ $backgroundClasses($errors->has($name)) }}"></div>
-        </label>
-
-        @if ($label)
-            <x-dynamic-component
-                :component="WireUi::component('label')"
-                :for="$id"
-                class="ml-2"
-                :label="$label"
-                :has-error="$errors->has($name)"
-            />
-        @endif
-    </div>
-
-    @if ($name)
-        <x-dynamic-component
-            :component="WireUi::component('error')"
-            :name="$name"
+    <label
+        class="relative flex items-center select-none group"
+        for="{{ $id }}"
+        tabindex="-1"
+    >
+        <input
+            {{ $attributes->merge([
+                'name' => $name,
+                'id'   => $id,
+            ])->class([
+                'translate-x-0 transform transition ease-in-out duration-200 cursor-pointer shadow',
+                'checked:bg-none peer focus:ring-0 focus:ring-offset-0 focus:outline-none bg-white',
+                'absolute mx-0.5 my-auto inset-y-0 rounded-full border-0 appearance-none',
+                'checked:text-white dark:bg-secondary-200',
+                data_get($sizeClasses, 'circle'),
+            ]) }}
+            type="checkbox"
         />
-    @endif
-</div>
+
+        <div @class([
+            'block rounded-full cursor-pointer transition ease-in-out duration-100',
+            'peer-focus:ring-2 peer-focus:ring-offset-2',
+            'group-focus:ring-2 group-focus:ring-offset-2',
+            data_get($sizeClasses, 'background'),
+            $colorClasses,
+
+            'invalidated:bg-negative-600 invalidated:peer-focus:bg-negative-600 invalidated:peer-focus:ring-negative-600 invalidated:dark:bg-negative-700',
+            'invalidated:group-focus:ring-negative-600 invalidated:dark:group-focus:ring-negative-700',
+            'invalidated:dark:peer-focus:ring-negative-700 invalidated:dark:peer-focus:ring-offset-secondary-800',
+        ])></div>
+    </label>
+</x-wrapper.switcher>
