00912bc040ae71c6600cf097ab026dcca99a8110
João Pedro Lopes <joaopalopes24@gmail.com>
2023-11-03 16:24:50 -0300

    test(todo): adding tests to do

---
+++ tests/Unit/View/Components/CheckboxTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests\Unit\View\Components;
+
+test('example', function () {
+    expect(true)->toBeTrue();
+})->todo();
---
+++ tests/Unit/View/Components/Select/UserOptionTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tests\Unit\View\Components\Select;
+
+test('example', function () {
+    expect(true)->toBeTrue();
+})->todo();
