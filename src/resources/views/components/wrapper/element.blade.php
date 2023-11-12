898f7763242a25fd4c960bdef1d85ca03655f86c
FelipeArnold <felipearnold.94@gmail.com>
2023-11-12 20:42:07 -0300

    Removed border duplication from input when in focus.

--- src/resources/views/components/wrapper/element.blade.php
+++ src/resources/views/components/wrapper/element.blade.php
@@ -6,6 +6,7 @@
     ])->class([
         'bg-transparent block w-full border-0 text-gray-900 dark:text-gray-400',
         'p-0 outline-0 ring-0 sm:text-sm sm:leading-6',
+        'focus:ring-0 focus:border-0',
         'placeholder:text-gray-400 dark:placeholder:text-gray-300',
         'invalidated:text-negative-800 invalidated:dark:text-negative-600',
         'invalidated:placeholder-negative-400 invalidated:dark:placeholder-negative-600/70',
