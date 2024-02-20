07e5c0ca83502c92be9c7a7e103cf01f777f2522
PH7-Jack <pedrolivertwd@gmail.com>
2024-02-20 03:53:07 -0300

    remove persistent from modal body

--- src/Components/Modal/views/index.blade.php
+++ src/Components/Modal/views/index.blade.php
@@ -49,7 +49,6 @@
             $alignClasses,
             $widthClasses,
         ])
-        @unless($persistent) x-on:click="close" @endunless
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
