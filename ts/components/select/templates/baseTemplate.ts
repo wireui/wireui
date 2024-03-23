c1674d7cd1b97a3bad5f848873ca2d4ad7c5e419
PH7-Jack <pedrolivertwd@gmail.com>
2024-03-22 23:17:53 -0300

    fix hover and focus icon color

--- ts/components/select/templates/baseTemplate.ts
+++ ts/components/select/templates/baseTemplate.ts
@@ -19,7 +19,7 @@ export default (slot: string) => `
 
     <template x-if="option.isSelected">
         <div class="flex-shrink-0">
-            <svg class="w-5 h-5 text-primary-600 dark:text-secondary-500 group-hover:text-white"
+            <svg class="w-5 h-5 text-primary-600 dark:text-secondary-500 group-hover:text-white group-focus:text-primary-600"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor">
