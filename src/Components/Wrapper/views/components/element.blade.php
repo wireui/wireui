cdf8f6c067dc814dda5e0388f078373acc55f823
PH7-Jack <pedrolivertwd@gmail.com>
2024-05-30 01:13:34 -0300

    fix outline

--- src/Components/Wrapper/views/components/element.blade.php
+++ src/Components/Wrapper/views/components/element.blade.php
@@ -5,7 +5,7 @@
         'placeholder'  => ' ',
     ])->class([
         'bg-transparent block w-full border-0 text-gray-900 dark:text-gray-400',
-        'p-0 outline-0 ring-0 sm:text-sm sm:leading-6',
+        'p-0 outline-none ring-0 sm:text-sm sm:leading-6',
         'focus:ring-0 focus:border-0',
         'placeholder:text-gray-400 dark:placeholder:text-gray-300',
         'invalidated:text-negative-800 invalidated:dark:text-negative-600',
