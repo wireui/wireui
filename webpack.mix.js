29e8a845fac1465f058157b0de238b370f07f41c
PH7-Jack <pedrolivertwd@gmail.com>
2024-06-01 12:47:55 -0300

    setup custom path to build and build-dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
+const buildPath = mix.inProduction() ? 'dist' : 'dist/dev'
+
 mix.ts('ts/index.ts', 'dist/wireui.js')
-  .setPublicPath('dist')
+  .setPublicPath(buildPath)
   .postCss('ts/global.css', 'dist/wireui.css', [require('tailwindcss')])
   .alias({
     '@': path.join(__dirname, 'ts')
