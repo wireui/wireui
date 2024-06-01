dca8e74d6f1df1a9b2feaa67c36c9c1a0e92f523
PH7-Jack <pedrolivertwd@gmail.com>
2024-06-01 13:45:35 -0300

    rollback custom build path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,8 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
-const buildPath = mix.inProduction() ? 'dist' : 'dist/dev'
-
 mix.ts('ts/index.ts', 'dist/wireui.js')
-  .setPublicPath(buildPath)
+  .setPublicPath('dist')
   .postCss('ts/global.css', 'dist/wireui.css', [require('tailwindcss')])
   .alias({
     '@': path.join(__dirname, 'ts')
