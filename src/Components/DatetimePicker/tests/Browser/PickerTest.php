19b2c272f553e7c77f8d82c0651660e9955fd40c
João Pedro Lopes <joaopalopes24@gmail.com>
2024-06-13 22:45:45 -0300

    test(datetime): adjust datetime tests

--- src/Components/DatetimePicker/tests/Browser/PickerTest.php
+++ src/Components/DatetimePicker/tests/Browser/PickerTest.php
@@ -9,110 +9,45 @@ use Tests\Browser\BrowserTestCase;
 
 class PickerTest extends BrowserTestCase
 {
-    // public function browser(): Browser
-    // {
-    //     return Livewire::visit(new class() extends Component
-    //     {
-    //         public $date = null;
+    public function browser(): Browser
+    {
+        return Livewire::visit(new class() extends Component
+        {
+            public $date = null;
 
-    //         public $model = '2021-12-15 10:30';
+            public $model = '2021-12-15T10:30:00';
 
-    //         public $dateAndTime = '2021-12-25 00:00';
+            public function mount(): void
+            {
+                $this->date = Carbon::parse('2021-12-15 10:30');
+            }
 
-    //         public $utcTimezone = '2021-07-22 00:30';
+            public function render(): string
+            {
+                return <<<'BLADE'
+                <div>
+                    <x-badge dusk="model" :label="$model" />
 
-    //         public $customFormat = '29-2021-09 59:13';
+                    <x-datetime-picker
+                        wire:model.live="model"
+                        label="Min Max Limits"
+                        without-timezone
+                        :min="$date->copy()->subDays(7)->setHour(12)->format('u')"
+                        :max="$date->copy()->addDays(7)->setHour(15)->format('u')"
+                    />
+                </div>
+                BLADE;
+            }
+        });
+    }
 
-    //         public $tokyoTimezone = '2021-07-26 10:00';
-
-    //         public $withoutTimezone = '2021-05-22T02:48';
-
-    //         public function mount(): void
-    //         {
-    //             $this->date = Carbon::parse('2021-12-15 10:30');
-    //         }
-
-    //         public function render(): string
-    //         {
-    //             return <<<'BLADE'
-    //             <div>
-    //                 <h1>Datetime Picker Browser Test</h1>
-
-    //                 // test it_should_select_date_without_timezone_difference
-    //                 <div id="withoutTimezone">
-    //                     <x-datetime-picker
-    //                         wire:model.live="withoutTimezone"
-    //                         without-timezone
-    //                         label="Without Timezone"
-    //                         display-format="YYYY-MM-DD HH:mm"
-    //                     />
-    //                     <span dusk="withoutTimezone">{{ $withoutTimezone }}</span>
-    //                 </div>
-
-    //                 // test it_should_select_date_with_utc_timezone_difference
-    //                 <div id="utcTimezone">
-    //                     <x-datetime-picker
-    //                         wire:model.live="utcTimezone"
-    //                         label="UTC Timezone"
-    //                         {{-- the user's timezone is automatic, but I need to mock the timezone in the tests --}}
-    //                         user-timezone="America/Sao_Paulo"
-    //                         display-format="YYYY-MM-DD HH:mm"
-    //                     />
-    //                     <span dusk="utcTimezone">{{ $utcTimezone }}</span>
-    //                 </div>
-
-    //                 // test it_should_select_date_with_default_timezone_and_auto_user_timezone
-    //                 <div id="tokyoTimezone">
-    //                     <x-datetime-picker
-    //                         wire:model.live="tokyoTimezone"
-    //                         timezone="Asia/Tokyo"
-    //                         {{-- the user's timezone is automatic, but I need to mock the timezone in the tests --}}
-    //                         user-timezone="America/Sao_Paulo"
-    //                         label="Asia/Tokyo Timezone"
-    //                         display-format="YYYY-MM-DD HH:mm"
-    //                     />
-    //                     <span dusk="tokyoTimezone">{{ $tokyoTimezone }}</span>
-    //                 </div>
-
-    //                 // test it_should_parse_date_in_custom_format
-    //                 <div id="customFormat">
-    //                     <x-datetime-picker
-    //                         wire:model.live="customFormat"
-    //                         parse-format="DD-YYYY-MM mm:HH"
-    //                         without-timezone
-    //                         label="Custom Format Parse"
-    //                         display-format="DD-YYYY-MM mm:HH"
-    //                     />
-    //                     <span dusk="customFormat">{{ $customFormat }}</span>
-    //                 </div>
-
-    //                 // test it_should_select_date_and_time
-    //                 <div id="dateAndTime">
-    //                     <x-datetime-picker
-    //                         wire:model.live="dateAndTime"
-    //                         without-timezone
-    //                         label="Date and Time"
-    //                         display-format="DD-MM-YYYY HH:mm"
-    //                     />
-    //                     <span dusk="dateAndTime">{{ $dateAndTime }}</span>
-    //                 </div>
-
-    //                 <h1>MinMaxLimitsTest</h1>
-
-    //                 <div id="minMaxLimits">
-    //                     <x-datetime-picker
-    //                         wire:model.live="model"
-    //                         without-timezone
-    //                         :min="$date->copy()->subDays(7)->setHour(12)->toISOString()"
-    //                         :max="$date->copy()->addDays(7)->setHour(15)->toISOString()"
-    //                     />
-    //                     <span dusk="value">{{ $model }}</span>
-    //                 </div>
-    //             </div>
-    //             BLADE;
-    //         }
-    //     });
-    // }
+    // <x-datetime-picker
+    //     wire:model.live="model"
+    //     label="Min Max Limits"
+    //     without-timezone
+    //     :min="$date->copy()->subDays(7)->setHour(12)->format('u')"
+    //     :max="$date->copy()->addDays(7)->setHour(15)->format('u')"
+    // />
 
     public function test_it_should_select_date_without_timezone_difference(): void
     {
@@ -246,65 +181,78 @@ class PickerTest extends BrowserTestCase
             ->waitForWrapperValue('10-2021-09 59:13');
     }
 
-    // public function test_it_should_select_date_and_time(): void
-    // {
-    //     $this->browser()
-    //         ->assertInputValue('dateAndTime', '25-12-2021 00:00')
-    //         ->click('[id="dateAndTime"] input')
-    //         ->tap(fn (Browser $browser) => $browser->selectDate('dateAndTime', 11))
-    //         ->tap(fn (Browser $browser) => $browser->waitForLivewire())
-    //         ->waitForTextIn('@dateAndTime', '2021-12-11T00:00:00Z')
-    //         ->assertInputValue('dateAndTime', '11-12-2021 00:00')
-    //         ->pause(1000)
-    //         ->tap(fn (Browser $browser) => $browser->script(<<<JS
-    //             console.log(document.querySelectorAll('[id="dateAndTime"] .picker-times button'));
+    public function test_it_should_select_date_and_time(): void
+    {
+        Livewire::visit(new class() extends Component
+        {
+            public $model = '2021-12-25T00:00:00';
 
-    //             [...document.querySelectorAll('[id="dateAndTime"] .picker-times button')]
-    //                 .find(time => time.innerText.includes('5:50 AM'))
-    //                 .click()
-    //         JS))
-    //         ->waitForTextIn('@dateAndTime', '2021-12-11T05:50:00Z')
-    //         ->assertInputValue('dateAndTime', '11-12-2021 05:50');
-    // }
+            public function render(): string
+            {
+                return <<<'BLADE'
+                <div>
+                    <x-badge dusk="model" :label="$model" />
 
-    // /**
-    //  * @test
-    //  *
-    //  * @dataProvider datesProvider
-    //  */
-    // public function it_should_select_only_the_dates_inside_a_range_min_and_max(bool $disabled, int $day, string $model, string $input)
-    // {
-    //     $browser = $this->browser()
-    //         ->click('[name="model"]')
-    //         ->tap(fn (Browser $browser) => $browser->assertScript(<<<EOT
-    //                 [...document.querySelectorAll('.picker-days button')]
-    //                     .find(day => day.innerText == {$day})
-    //                     .hasAttribute('disabled')
-    //             EOT, $disabled))
-    //         ->tap(fn (Browser $browser) => $browser->selectDate('minMaxLimits', $day));
+                    <x-datetime-picker
+                        wire:model.live="model"
+                        label="Date and Time"
+                        without-timezone
+                        display-format="DD-MM-YYYY HH:mm"
+                    />
+                </div>
+                BLADE;
+            }
+        })
+            ->assertInputValue('model', '2021-12-25T00:00:00')
+            ->toggleWrapper()
+            ->tap(fn (Browser $browser) => $browser->selectDate('model', 11))
+            ->waitForTextIn('@model', '2021-12-11T00:00:00')
+            ->downTimePicker('hours', 4)
+            ->downTimePicker('minutes', 16)
+            ->downTimePicker('seconds', 21)
+            ->assertInputValue('model', '2021-12-11T08:44:39')
+            ->downTimePicker('period', 1)
+            ->assertInputValue('model', '2021-12-11T20:44:39')
+            ->toggleWrapper()
+            ->waitForWrapperValue('11-12-2021 20:44');
+    }
 
-    //     if (!$disabled) {
-    //         $browser
-    //             ->waitForTextIn('@value', $model)
-    //             ->assertInputValue('model', $input);
-    //     }
-    // }
+    /**
+     * @test
+     *
+     * @dataProvider datesProvider
+     */
+    public function it_should_select_only_the_dates_inside_a_range_min_and_max(bool $disabled, int $day, string $model, string $input)
+    {
+        $browser = $this->browser()
+            ->toggleWrapper()
+            ->tap(fn (Browser $browser) => $browser->selectDate('model', $day))
+            ->pause(1000000);
 
-    // /**
-    //  * @test
-    //  *
-    //  * @dataProvider timesProvider
-    //  */
-    // public function it_should_select_only_times_inside_the_limit(int $day, string $time, bool $exists)
-    // {
-    //     $this->browser()
-    //         ->click('[name="model"]')
-    //         ->tap(fn (Browser $browser) => $browser->selectDate('minMaxLimits', $day))
-    //         ->waitUsing(7, 100, fn (Browser $browser) => $browser->assertScript(
-    //             "!!document.querySelector('[name=\"times.{$time}\"]')",
-    //             $exists,
-    //         ));
-    // }
+        // if (!$disabled) {
+        //     $browser
+        //         ->waitForTextIn('@value', $model)
+        //         ->assertInputValue('model', $input);
+        // }
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider timesProvider
+     */
+    public function it_should_select_only_times_inside_the_limit(int $day, string $time, bool $exists)
+    {
+        $this->browser()
+            ->toggleWrapper()
+            ->tap(fn (Browser $browser) => $browser->selectDate('model', $day))
+            ->pause(1000000000);
+
+            // ->waitUsing(7, 100, fn (Browser $browser) => $browser->assertScript(
+            //     "!!document.querySelector('[name=\"times.{$time}\"]')",
+            //     $exists,
+            // ));
+    }
 
     public static function datesProvider(): array
     {
