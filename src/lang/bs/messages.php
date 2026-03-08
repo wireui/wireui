f246e24d7bab8c60fef549095934cd8eaae8b8b8
Nenad Gajic <nenad_gajic@outlook.com>
2026-03-08 20:09:17 +0100

    feat(lang): add missing translations

--- src/lang/bs/messages.php
+++ src/lang/bs/messages.php
@@ -11,8 +11,15 @@ return [
         'tomorrow' => 'Sutra',
         'today' => 'Danas',
         'yesterday' => 'Juče',
+        'clear' => 'Očisti',
+        'close' => 'Zatvori',
+        'apply' => 'Primeni',
+        'cancel' => 'Otkaži',
     ],
     'errors' => [
-        'title' => 'Postoje {errors} greške u unosu',
+        'title' => 'Došlo je do greške u unosu|Došlo je do {errors} grešaka u unosu',
+    ],
+    'labels' => [
+        'remove' => 'Ukloni',
     ],
 ];
