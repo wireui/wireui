0f0ab2b10627ad11ba672a6f43bf10a327849b91
Nenad Gajic <nenad_gajic@outlook.com>
2026-03-08 19:54:30 +0100

    feat(lang): add Serbian (sr) translations

---
+++ src/lang/sr/messages.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'select_time' => 'Izaberi vreme',
+    'empty_options' => 'Nema rezultata',
+    'loading' => 'Učitavanje...',
+    'search_here' => 'Potraži ovde',
+    'date_picker' => [
+        'months' => ['Januar', 'Februar', 'Mart', 'April', 'Maj', 'Jun', 'Jul', 'Avgust', 'Septembar', 'Oktobar', 'Novembar', 'Decembar'],
+        'days' => ['Ned', 'Pon', 'Uto', 'Sri', 'Čet', 'Pet', 'Sub'],
+        'tomorrow' => 'Sutra',
+        'today' => 'Danas',
+        'yesterday' => 'Juče',
+        'clear' => 'Očisti',
+        'close' => 'Zatvori',
+        'apply' => 'Primeni',
+        'cancel' => 'Otkaži',
+    ],
+    'errors' => [
+        'title' => 'There was an error in your submission|There were {errors} errors in your submission',
+        'title' => 'Došlo je do greške u unosu|Došlo je do {errors} grešaka u unosu',
+    ],
+    'labels' => [
+        'remove' => 'Ukloni',
+    ],
+];
