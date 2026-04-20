632df5b29871cfb8ccd1130c62b271e71e01b455
hawraz93 <hawrazn1@gmail.com>
2026-04-20 13:21:32 +0300

    feat: enhance RTL/LTR support with Arabic-Indic digit normalization and logical properties

--- src/Components/TextField/views/number.blade.php
+++ src/Components/TextField/views/number.blade.php
@@ -1,51 +1,25 @@
-<x-dynamic-component
-    :component="WireUi::component('text-field')"
-    :config="$config"
-    :attributes="$wrapper"
-    x-data="wireui_inputs_number"
-    :x-props="WireUi::toJs([
+<x-dynamic-component :component="WireUi::component('text-field')" :config="$config" :attributes="$wrapper"
+    x-data="wireui_inputs_number" :x-props="WireUi::toJs([
         'disabled' => $disabled,
         'readonly' => $readonly,
-    ])"
->
+    ])">
     @include('wireui-wrapper::components.slots', [
-        'except' => ['prepend', 'append'],
+    'except' => ['prepend', 'append'],
     ])
 
     <x-slot:prepend>
-        <x-dynamic-component
-            :component="WireUi::component('button')"
-            :color="$color ?? 'primary'"
-            :rounded="data_get($roundedClasses, 'prepend', '')"
-            x-hold.click.delay.repeat.100ms="minus"
-            x-bind:disabled="disableMinus"
-            x-on:keydown.enter="minus"
-            use-validation-colors
-            :icon="$icon"
-            flat
-        />
+        <x-dynamic-component :component="WireUi::component('button')" :color="$color ?? 'primary'"
+            :rounded="data_get($roundedClasses, 'prepend', '')" x-hold.click.delay.repeat.100ms="minus"
+            x-bind:disabled="disableMinus" x-on:keydown.enter="minus" use-validation-colors :icon="$icon" flat />
     </x-slot:prepend>
 
-    <x-wireui-wrapper::element
-        type="number"
-        x-ref="input"
-        inputmode="numeric"
-        x-on:keydown.up.stop.prevent="plus"
-        x-on:keydown.down.stop.prevent="minus"
-        :attributes="$input->class('text-center appearance-number-none')"
-    />
+    <x-wireui-wrapper::element type="text" x-ref="input" inputmode="numeric" x-on:keydown.up.stop.prevent="plus"
+        x-on:keydown.down.stop.prevent="minus" x-on:beforeinput="handleBeforeInput($event)"
+        :attributes="$input->class('text-center appearance-number-none')" />
 
     <x-slot:append>
-        <x-dynamic-component
-            :component="WireUi::component('button')"
-            x-hold.click.delay.repeat.100ms="plus"
-            :color="$color ?? 'primary'"
-            :rounded="data_get($roundedClasses, 'append', '')"
-            x-bind:disabled="disablePlus"
-            x-on:keydown.enter="plus"
-            use-validation-colors
-            :icon="$rightIcon"
-            flat
-        />
+        <x-dynamic-component :component="WireUi::component('button')" x-hold.click.delay.repeat.100ms="plus"
+            :color="$color ?? 'primary'" :rounded="data_get($roundedClasses, 'append', '')"
+            x-bind:disabled="disablePlus" x-on:keydown.enter="plus" use-validation-colors :icon="$rightIcon" flat />
     </x-slot:append>
 </x-dynamic-component>
